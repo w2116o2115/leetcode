061434cfdcd881168aca82d557da6fa8b19e3e59
zhangwei112 <zhangwei112@jd.com>
2019-07-30 10:54:13 +0800

    呵呵

--- problems/src/array/ThirdMaximumNumber$E.java
+++ problems/src/array/ThirdMaximumNumber$E.java
@@ -29,25 +29,24 @@ public class ThirdMaximumNumber$E {
    * @throws Exception
    */
   public static void main(String[] args) throws Exception {
-    int[] a = {1, 2};
+    int[] a = {1, 2,3};
     System.out.println(new ThirdMaximumNumber$E().thirdMax(a));
   }
 
   public int thirdMax(int[] nums) {
     long[] max = {Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE};
     int count = 0;
-    for (int num : nums) {
-      for (int j = 0; j < 3; j++) {
+    for (int num : nums){
+      for (int j=0;j<3;j++){
         if (max[j] > num) continue;
         else if (max[j] == num) break;
+        count ++;
         int k = j;
-        long temp1, temp2;
-        temp1 = num;
-        count++;
-        while (k < 3) {
-          temp2 = max[k];
+        long temp,temp1 = num;
+        while (k < 3){
+          temp = max[k];
           max[k] = temp1;
-          temp1 = temp2;
+          temp1 = temp;
           k++;
         }
         break;
