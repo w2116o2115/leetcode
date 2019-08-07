b6ca96f9e88caa9e42a6f0a86ff95fe6cef8e8ef
zhangwei112 <zhangwei112@jd.com>
2019-08-07 17:44:07 +0800

    呵呵

--- problems/src/array/NextGreaterElementI$E.java
+++ problems/src/array/NextGreaterElementI$E.java
@@ -1,22 +1,13 @@
 package array;
 
 /**
- * Created by gouthamvidyapradhan on 09/02/2018. You are given two arrays (without duplicates) nums1
- * and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for
- * nums1's elements in the corresponding places of nums2.
- *
- * <p>The Next Greater Number of a number x in nums1 is the first greater number to its right in
- * nums2. If it does not exist, output -1 for this number.
- *
- * <p>Example 1: Input: nums1 = [4,1,2], nums2 = [1,3,4,2]. Output: [-1,3,-1] Explanation: For
- * number 4 in the first array, you cannot find the next greater number for it in the second array,
- * so output -1. For number 1 in the first array, the next greater number for it in the second array
- * is 3. For number 2 in the first array, there is no next greater number for it in the second
- * array, so output -1. Example 2: Input: nums1 = [2,4], nums2 = [1,2,3,4]. Output: [3,-1]
- * Explanation: For number 2 in the first array, the next greater number for it in the second array
- * is 3. For number 4 in the first array, there is no next greater number for it in the second
- * array, so output -1. Note: All elements in nums1 and nums2 are unique. The length of both nums1
- * and nums2 would not exceed 1000.
+ * 给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ *  输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ *  输出: [-1,3,-1]
+ *  解释:
+ *  对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+ *  对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+ *  对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
  */
 public class NextGreaterElementI$E {
 
