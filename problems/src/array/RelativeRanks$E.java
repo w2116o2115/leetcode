3a71972cb96d0d7d6f03da72e76dcb6d923c6a59
zhangwei112 <zhangwei112@jd.com>
2019-08-07 11:35:31 +0800

    呵呵

--- problems/src/array/RelativeRanks$E.java
+++ problems/src/array/RelativeRanks$E.java
@@ -13,6 +13,14 @@ import java.util.List;
  * Medal", "Silver Medal" and "Bronze Medal". For the left two athletes, you just need to output
  * their relative ranks according to their scores. Note: N is a positive integer and won't exceed
  * 10,000. All the scores of athletes are guaranteed to be unique.
+ *
+ * 根据N名运动员的得分，找到他们的相对等级和得分最高的三个人，他们将获得奖牌：“金牌”，“银牌”和“铜牌”。例如：
+ *
+ * 输入：[5,4,3,2,1]
+ *
+ * 输出：["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+ *
+ * 说明：前三名运动员获得前三名最高分，因此获得“Gold Medal”，“Silver Medal”和“Bronze Medal”。对于剩下的两名运动员，你只需要根据他们的分数输出他们的相对等级。
  */
 public class RelativeRanks$E {
 
