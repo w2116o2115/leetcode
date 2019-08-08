5b10a4d67d8333b528c08f5ede7a0408e8707188
zhangwei112 <zhangwei112@jd.com>
2019-08-08 18:43:55 +0800

    呵呵

---
+++ problems/src/array/AddToArrayFormOfInteger$E.java
@@ -0,0 +1,44 @@
+package array;
+
+import java.math.BigInteger;
+import java.util.*;
+
+/**
+ * 给定非负整数X的数组形式A，返回整数X + K的数组形式。例如：
+ *
+ * 输入：A = [1,2,0,0]，K = 34
+ * 输出：[1,2,3,4]
+ * 说明：1200 + 34 = 1234
+ *
+ * 输入：A = [2,7,4]，K = 181
+ * 输出：[4,5,5]
+ * 说明：274 + 181 = 455
+ *
+ * 输入：A = [2,1,5]，K = 806
+ * 输出：[1,0,2,1]
+ * 说明：215 + 806 = 1021
+ *
+ * 输入：A = [9,9,9,9,9,9,9,9,9,9]，K = 1
+ * 输出：[1,0,0,0,0,0,0,0,0,0,0]
+ * 说明：9999999999 + 1 = 10000000000
+ */
+public class AddToArrayFormOfInteger$E {
+  public static void main(String[] args) {
+    //
+  }
+
+  public List<Integer> addToArrayForm(int[] A, int K) {
+    StringBuilder sb = new StringBuilder();
+    for (int a : A) {
+      sb.append(a);
+    }
+    BigInteger big = new BigInteger(sb.toString());
+    BigInteger result = big.add(BigInteger.valueOf(K));
+    String resultStr = result.toString();
+    List<Integer> list = new ArrayList<>();
+    for (char a : resultStr.toCharArray()) {
+      list.add(Integer.parseInt(String.valueOf(a)));
+    }
+    return list;
+  }
+}
