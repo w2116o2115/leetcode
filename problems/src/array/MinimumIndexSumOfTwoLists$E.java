7f3972f849ffd38217d9316fe24ec19bc74d436c
zhangwei112 <zhangwei112@jd.com>
2019-08-08 18:20:15 +0800

    呵呵

--- problems/src/array/MinimumIndexSumOfTwoLists$E.java
+++ problems/src/array/MinimumIndexSumOfTwoLists$E.java
@@ -52,13 +52,16 @@ public class MinimumIndexSumOfTwoLists$E {
     for (int i=0;i<list1.length;i++){
       for (int j=0;j<list2.length;j++){
         if (list1[i].equals(list2[j])){
-          if (min > i+j || min == i+j){
+          if (min > i+j){
             min = i+j;
+            result.clear();
+            result.add(list1[i]);
+          }else if (min == i+j){
             result.add(list1[i]);
           }
         }
       }
     }
-    return result.toArray(new String[result.size()]);
+    return result.toArray(new String[0]);
   }
 }
