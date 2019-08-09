1d839fbb32b9683ad9fcdc30d2d61f5d7e302dd7
zhangwei112 <zhangwei112@jd.com>
2019-08-09 11:28:12 +0800

    呵呵

--- problems/src/array/SetMatrixZeroes$M.java
+++ problems/src/array/SetMatrixZeroes$M.java
@@ -4,16 +4,39 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Created by pradhang on 3/28/2017. Given a m x n matrix, if an element is 0, set its entire row
- * and column to 0. Do it in place.
+ * 矩阵置零
+ * 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
  *
- * <p>click to show follow up.
+ * 示例 1:
  *
- * <p>Follow up: Did you use extra space? A straight forward solution using O(mn) space is probably
- * a bad idea. A simple improvement uses O(m + n) space, but still not the best solution. Could you
- * devise a constant space solution?
+ * 输入:
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * 输出:
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ * 示例 2:
+ *
+ * 输入:
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * 输出:
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
  */
-public class SetMatrixZeroes {
+public class SetMatrixZeroes$M {
   /**
    * Main method
    *
@@ -23,7 +46,7 @@ public class SetMatrixZeroes {
   public static void main(String[] args) throws Exception {
     int[][] matrix = {{0, 8, 7}, {9, 0, 8}, {9, 9, 0}};
 
-    new SetMatrixZeroes().setZeroes(matrix);
+    new SetMatrixZeroes$M().setZeroes(matrix);
   }
 
   public void setZeroes(int[][] matrix) {
