4714430918ca83c1488f0c2134fa99a83bb0ddd7
zhangwei112 <zhangwei112@jd.com>
2019-08-09 11:07:59 +0800

    呵呵

--- problems/src/array/ProductOfArrayExceptSelf$M.java
+++ problems/src/array/ProductOfArrayExceptSelf$M.java
@@ -10,6 +10,11 @@ package array;
  * 输入: [1,2,3,4]
  * 输出: [24,12,8,6]
  * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ *
+ * 思路
+ * 我们想，对于某一个数字，如果我们知道其前面所有数字的乘积，同时也知道后面所有的数乘积，那么二者相乘就是我们要的结果，
+ * 所以我们只要分别创建出这两个数组即可，分别从数组的两个方向遍历就可以分别创建出乘积累积数组
+ *
  */
 public class ProductOfArrayExceptSelf$M {
   public static void main(String[] args) {
@@ -20,13 +25,13 @@ public class ProductOfArrayExceptSelf$M {
 
   public int[] productExceptSelf(int[] nums) {
     int[] result = new int[nums.length];
-    for (int i = 0, temp = 1, l = nums.length; i < l; i++) {
+    for (int i=0,temp = 1;i<nums.length;i++){
       result[i] = temp;
-      temp *= nums[i];
+      temp = nums[i] * temp;
     }
-    for (int i = nums.length - 1, temp = 1; i >= 0; i--) {
-      result[i] = result[i] * temp;
-      temp *= nums[i];
+    for (int j=nums.length-1,temp = 1;j>=0;j--){
+      result[j] = result[j]*temp;
+      temp *= nums[j];
     }
     return result;
   }
