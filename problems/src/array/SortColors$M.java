d80a9d404cb2fa07a68be05702abb426a19cd18b
zhangwei112 <zhangwei112@jd.com>
2019-08-12 15:39:32 +0800

    呵呵

--- problems/src/array/SortColors$M.java
+++ problems/src/array/SortColors$M.java
@@ -1,25 +1,19 @@
 package array;
 
 /**
- * Created by gouthamvidyapradhan on 06/08/2017. Given an array with n objects colored red, white or
- * blue, sort them so that objects of the same color are adjacent, with the colors in the order red,
- * white and blue.
+ * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
  *
- * <p>Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue
- * respectively.
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
  *
- * <p>Note: You are not suppose to use the library's sort function for this problem.
+ * 注意:
+ * 不能使用代码库中的排序函数来解决这道题。
  *
- * <p>Follow up: A rather straight forward solution is a two-pass algorithm using counting sort.
- * First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total
- * number of 0's, then 1's and followed by 2's.
+ * 示例:
  *
- * <p>Could you come up with an one-pass algorithm using only constant space?
- *
- * <p>Solution: The below solution works with one pass. The basic idea is to keep track of start and
- * end index of contiguous 1s and push the 0s to left of 1s and 2 to right of 1s.
+ * 输入: [2,0,2,1,1,0]
+ * 输出: [0,0,1,1,2,2]
  */
-public class SortColors {
+public class SortColors$M {
   /**
    * Main method
    *
@@ -28,7 +22,7 @@ public class SortColors {
    */
   public static void main(String[] args) throws Exception {
     int[] nums = {2, 1, 0, 0, 1};
-    new SortColors().sortColors(nums);
+    new SortColors$M().sortColors(nums);
     for (int i : nums) System.out.println(i);
   }
 
