57918f71fe600e74e87eadbe88859139b740108f
zhangwei112 <zhangwei112@jd.com>
2019-08-15 10:41:34 +0800

    呵呵

--- problems/src/array/CardFilipGame$M.java
+++ problems/src/array/CardFilipGame$M.java
@@ -5,22 +5,19 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by gouthamvidyapradhan on 04/05/2018.
+ * 桌上有N张牌，正反面各有一个正整数（正反面的数可能不同）。
  *
- * <p>On a table are N cards, with a positive integer printed on the front and back of each card
- * (possibly different).
+ * 我们翻任意数量的牌，然后选一张牌。
  *
- * <p>We flip any number of cards, and after we choose one card.
+ * 如果选择的这张牌背面的数字X不在桌面上的牌的数字里，X就是好数。
  *
- * <p>If the number X on the back of the chosen card is not on the front of any card, then this
- * number X is good.
+ * 问最小的好数是几，如果没有，就返回0。
  *
- * <p>What is the smallest number that is good? If no number is good, output 0.
+ * fronts[i] 和 backs[i] 代表桌面上第i张牌的正面和反面数字。
  *
- * <p>Here, fronts[i] and backs[i] represent the number on the front and back of card i.
+ * 一次翻牌调换牌的正反面数字，翻完以后原来冲上的数字到了下面，原来冲下的数字到了上面。
  *
- * <p>A flip swaps the front and back numbers, so the value on the front is now on the back and vice
- * versa.
+ * 思路 ： 找到最小的数字，没有任何一张牌的正反面都是这个数字，这个数字就是最小的好数。
  *
  * <p>Example:
  *
@@ -32,11 +29,13 @@ import java.util.List;
  *
  * <p>1 <= fronts.length == backs.length <= 1000. 1 <= fronts[i] <= 2000. 1 <= backs[i] <= 2000.
  */
-public class CardFilipGame {
+public class CardFilipGame$M {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    System.out.println(flipgame(new int[]{1,2,4,4,7},new int[]{1,3,4,1,3}));
+  }
 
-  public int flipgame(int[] fronts, int[] backs) {
+  public static int flipgame(int[] fronts, int[] backs) {
     List<Integer> numbers = new ArrayList<>();
     for (int i = 0; i < fronts.length; i++) {
       numbers.add(fronts[i]);
