7f2fcb0d36aeaa8ed51f12207f37fc52f49edd50
zhangwei112 <zhangwei112@jd.com>
2019-08-19 14:26:03 +0800

    呵呵

--- problems/src/backtracking/LetterCasePermutation$M.java
+++ problems/src/backtracking/LetterCasePermutation$M.java
@@ -2,6 +2,7 @@ package backtracking;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 给定一个字符串S，通过将字符串S中的每个字母转变大小写，我们可以获得一个新的字符串。返回所有可能得到的字符串集合。
@@ -21,37 +22,45 @@ import java.util.List;
  * S 仅由数字和字母组成。
  */
 public class LetterCasePermutation$M {
-
-  /**
-   * Main method
-   *
-   * @param args
-   * @throws Exception
-   */
-  public static void main(String[] args) throws Exception {
-    System.out.println(new LetterCasePermutation$M().letterCasePermutation("a1b2"));
-  }
-
-  public List<String> letterCasePermutation(String S) {
-    List<String> result = new ArrayList<>();
-    backtrack(S, result, 0, "");
-    return result;
-  }
-
-  private void backtrack(String s, List<String> result, int i, String r) {
-    if (i == s.length()) {
-      result.add(r);
-    } else {
-      if (Character.isAlphabetic(s.charAt(i))) {
-        backtrack(s, result, i + 1, r + s.charAt(i));
-        if (Character.isLowerCase(s.charAt(i))) {
-          backtrack(s, result, i + 1, r + Character.toUpperCase(s.charAt(i)));
-        } else {
-          backtrack(s, result, i + 1, r + Character.toLowerCase(s.charAt(i)));
-        }
-      } else {
-        backtrack(s, result, i + 1, r + s.charAt(i));
-      }
+  public static List<String> letterCasePermutation(String S) {
+    List<String> res = new ArrayList<>();
+    // 特判
+    int len = S.length();
+    if (len == 0) {
+      return res;
     }
+    Stack<Character> path = new Stack<>();
+    dfs(S, 0, len, path, res);
+    return res;
+  }
+
+  private static void dfs(String S, int index, int len, Stack<Character> stack, List<String> res) {
+    if (index == len) {
+      StringBuilder stringBuilder = new StringBuilder();
+      for (int i = 0; i < len; i++) {
+        stringBuilder.append(stack.get(i));
+      }
+      res.add(stringBuilder.toString());
+      return;
+    }
+
+    stack.add(S.charAt(index));
+    dfs(S, index + 1, len, stack, res);
+    stack.pop();
+
+    if (Character.isLetter(S.charAt(index))) {
+      stack.add((char) (S.charAt(index) ^ (1 << 5)));
+      dfs(S, index + 1, len, stack, res);
+      stack.pop();
+    }
+  }
+
+  public static void main(String[] args) {
+//    String S = "a1b2";
+//    List<String> letterCasePermutation = letterCasePermutation(S);
+//    System.out.println(letterCasePermutation);
+
+    char a = 'a';
+    System.out.println((char)(a ^ (1<<5)));
   }
 }
