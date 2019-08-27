9b81855408e6e9616c8e0373289d0dfdfc409b56
zhangwei112 <zhangwei112@jd.com>
2019-08-27 15:58:07 +0800

    呵呵

--- problems/src/binary_search/SearchForARange$M.java
+++ problems/src/binary_search/SearchForARange$M.java
@@ -19,10 +19,9 @@ package binary_search;
 public class SearchForARange$M {
   public static void main(String[] args) throws Exception {
     int[] test = {
-      5, 7, 7, 8, 8, 10, 10, 10, 10, 18, 19, 20, 21, 21, 21, 21, 22, 23, 28, 28, 90, 101, 101, 101,
-      200, 200, 200, 200, 200, 200
+            5,7,7,8,8,10
     };
-    int[] result = new SearchForARange$M().searchRange(test, 200);
+    int[] result = new SearchForARange$M().searchRange(test, 8);
     for (int i : result) System.out.print(i + " ");
   }
 
@@ -45,18 +44,23 @@ public class SearchForARange$M {
    */
   private int findIndex(int[] nums, int target, boolean isLowerIndex) {
     int result = -1;
-    int s = 0, e = nums.length - 1;
-    while (s <= e) {
-      int m = s + (e - s) / 2;
-      if (nums[m] == target) {
-        result = m;
-        if (isLowerIndex)
-          e = m - 1; // if searching for the lower index then search the lower bound,
-        // else search the upper bound
-        else s = m + 1;
-      } else if (nums[m] < target) {
-        s = m + 1;
-      } else e = m - 1;
+    int left = 0,right = nums.length - 1;
+    while (left <= right){
+      int mid = (left + right) >>> 1;
+      if (nums[mid] == target){
+        result = mid;
+        if (!isLowerIndex) {
+          left = mid + 1;
+        }else {
+          right = mid - 1;
+        }
+      }else {
+        if (nums[mid] > target){
+          right = mid - 1;
+        }else {
+          left = mid + 1;
+        }
+      }
     }
     return result;
   }
