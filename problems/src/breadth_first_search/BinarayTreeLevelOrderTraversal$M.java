b594131fa546c126b1c39888d877fcca2c4c9d11
zhangwei112 <zhangwei112@jd.com>
2019-08-28 14:15:18 +0800

    呵呵

--- problems/src/breadth_first_search/BinarayTreeLevelOrderTraversal$M.java
+++ problems/src/breadth_first_search/BinarayTreeLevelOrderTraversal$M.java
@@ -80,18 +80,17 @@ public class BinarayTreeLevelOrderTraversal$M {
   private List<List<Integer>> levels = new ArrayList<>();
 
   private void helper(TreeNode node, int level) {
-    // start the current level
-    if (levels.size() == level)
-      levels.add(new ArrayList<Integer>());
-
-    // fulfil the current level
+    if (levels.size() == level){
+      levels.add(new ArrayList<>());
+    }
     levels.get(level).add(node.val);
 
-    // process child nodes for the next level
-    if (node.left != null)
-      helper(node.left, level + 1);
-    if (node.right != null)
-      helper(node.right, level + 1);
+    if (node.left != null){
+      helper(node.left,level+1);
+    }
+    if (node.right != null){
+      helper(node.right,level+1);
+    }
   }
 
   private List<List<Integer>> levelOrder(TreeNode root) {
