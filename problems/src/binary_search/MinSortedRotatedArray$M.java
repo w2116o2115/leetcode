2a16c7c40afb2f0ff604d5a2cc74c5877ea74ae3
zhangwei112 <zhangwei112@jd.com>
2019-08-28 10:48:52 +0800

    呵呵

---
+++ problems/src/binary_search/MinSortedRotatedArray$M.java
@@ -0,0 +1,49 @@
+package binary_search;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 请找出其中最小的元素。
+ *
+ * 你可以假设数组中不存在重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,4,5,1,2]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,5,6,7,0,1,2]
+ * 输出: 0
+ */
+public class MinSortedRotatedArray$M {
+  /**
+   * Main method
+   *
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    int[] A = {3,4,5,1,2};
+    System.out.println(new MinSortedRotatedArray$M().findMin(A));
+  }
+
+  public int findMin(int[] nums) {
+    return findMinNum(nums);
+  }
+
+  public int findMinNum(int[] nums){
+    int left = 0,right = nums.length - 1;
+    while (left < right){
+      int mid = (left + right) >>> 1;
+      if (nums[mid] < left){
+        right = mid;
+      }else {
+        left = mid+1;
+      }
+    }
+    return left;
+  }
+}
