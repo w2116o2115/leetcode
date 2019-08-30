9b34eb3e9afa39bee73fe34876f71b86a59972b0
zhangwei112 <zhangwei112@jd.com>
2019-08-30 11:12:44 +0800

    呵呵

--- problems/src/breadth_first_search/Matrix$M.java
+++ problems/src/breadth_first_search/Matrix$M.java
@@ -1,49 +1,48 @@
 package breadth_first_search;
 
+import javafx.util.Pair;
+
 import java.util.*;
 
 /**
- * Created by gouthamvidyapradhan on 14/03/2019 Given a matrix consists of 0 and 1, find the
- * distance of the nearest 0 for each cell.
+ * 给定一个由 0 和 1 组成的矩阵，找出每个元素到最近的 0 的距离。
  *
- * <p>The distance between two adjacent cells is 1. Example 1: Input:
+ * 两个相邻元素间的距离为 1 。
  *
- * <p>0 0 0 0 1 0 0 0 0 Output: 0 0 0 0 1 0 0 0 0 Example 2: Input:
+ * 示例 1:
+ * 输入:
  *
- * <p>0 0 0 0 1 0 1 1 1 Output: 0 0 0 0 1 0 1 2 1 Note: The number of elements of the given matrix
- * will not exceed 10,000. There are at least one 0 in the given matrix. The cells are adjacent in
- * only four directions: up, down, left and right.
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 输出:
  *
- * <p>Solution: Add all the 0th cell to the queue and do a multi-source bfs to count the minimum
- * distance
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 示例 2:
+ * 输入:
+ *
+ * 0 0 0
+ * 0 1 0
+ * 1 1 1
+ * 输出:
+ *
+ * 0 0 0
+ * 0 1 0
+ * 1 2 1
  */
 public class Matrix$M {
-  private static class Node {
-    int r, c;
-    int d;
+  public static class Cell{
+    int x;
+    int y;
 
-    Node(int r, int c) {
-      this.r = r;
-      this.c = c;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (!(o instanceof Node)) return false;
-      Node node = (Node) o;
-      return r == node.r && c == node.c;
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(r, c);
+    public Cell(int x, int y) {
+      this.x = x;
+      this.y = y;
     }
   }
 
-  private final int[] R = {0, 0, 1, -1};
-  private final int[] C = {1, -1, 0, 0};
-  private Set<Node> done;
   /**
    * Main method
    *
@@ -52,39 +51,53 @@ public class Matrix$M {
   public static void main(String[] args) {
     int[][] temp = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
     int[][] result = new Matrix$M().updateMatrix(temp);
-    System.out.println();
+    for (int[] a : result){
+        System.out.println(Arrays.toString(a));
+    }
   }
 
   public int[][] updateMatrix(int[][] matrix) {
-    int[][] temp = new int[matrix.length][matrix[0].length];
-    done = new HashSet<>();
-    Queue<Node> queue = new ArrayDeque<>();
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
-        temp[i][j] = matrix[i][j];
-        if (matrix[i][j] == 0) {
-          Node node = new Node(i, j);
-          queue.offer(node);
-          done.add(node);
-        }
-      }
-    }
-    while (!queue.isEmpty()) {
-      Node curr = queue.poll();
-      for (int i = 0; i < 4; i++) {
-        int newR = curr.r + R[i];
-        int newC = curr.c + C[i];
-        if (newR >= 0 && newR < matrix.length && newC >= 0 && newC < matrix[0].length) {
-          Node child = new Node(newR, newC);
-          if (!done.contains(child)) {
-            done.add(child);
-            child.d = curr.d + 1;
-            temp[newR][newC] = child.d;
-            queue.offer(child);
-          }
-        }
+    int[][] temp = matrix.clone();
+
+    for (int i=0;i<matrix.length;i++){
+      for (int j=0;j<matrix[0].length;j++){
+        if (matrix[i][j] == 1)
+          temp[i][j] = findMinZero(matrix,i,j);
       }
     }
     return temp;
+  }
+
+  private int findMinZero(int[][] matrix,int x,int y){
+    Queue<Pair<Cell,Integer>> queue = new LinkedList<>();
+
+    Set<String> visited = new HashSet<>();
+
+    queue.add(new Pair<>(new Cell(x,y),0));
+
+    while (!queue.isEmpty()){
+      Pair<Cell,Integer> pair = queue.remove();
+
+      if (matrix[pair.getKey().x][pair.getKey().y] == 0)
+        return pair.getValue();
+
+      if (!visited.contains(pair.getKey().x + "#" + pair.getKey().y)){
+        visited.add(pair.getKey().x + "#" + pair.getKey().y);
+        getNextLevl(pair.getKey(),queue,pair.getValue()+1,matrix);
+      }
+    }
+    return matrix[x][y];
+  }
+
+  private void getNextLevl(Cell cell,Queue<Pair<Cell,Integer>> queue,Integer level,int[][] matrix) {
+    //一个点的上下左右
+    if ((cell.x - 1) >= 0)
+      queue.add(new Pair<>(new Cell(cell.x-1,cell.y),level));
+    if (cell.x + 1 < matrix.length)
+      queue.add(new Pair<>(new Cell(cell.x+1,cell.y),level));
+    if (cell.y - 1 >= 0)
+      queue.add(new Pair<>(new Cell(cell.x,cell.y-1),level));
+    if (cell.y + 1 < matrix[0].length)
+      queue.add(new Pair<>(new Cell(cell.x,cell.y+1),level));
   }
 }
