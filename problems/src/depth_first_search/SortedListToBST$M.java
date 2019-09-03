6a65fb5d36401d8495a30afad2d7f164ff652bd8
zhangwei112 <zhangwei112@jd.com>
2019-09-03 10:01:38 +0800

    呵呵

--- problems/src/depth_first_search/SortedListToBST$M.java
+++ problems/src/depth_first_search/SortedListToBST$M.java
@@ -3,38 +3,42 @@ package depth_first_search;
 import java.util.List;
 
 /**
- *
  * 109. 有序链表转换二叉搜索树
- *
+ * <p>
  * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
- *
+ * <p>
  * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 给定的有序链表： [-10, -3, 0, 5, 9],
- *
+ * <p>
  * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
- *
- *       0
- *      / \
- *    -3   9
- *    /   /
- *  -10  5
- *
+ * <p>
+ * 0
+ * / \
+ * -3   9
+ * /   /
+ * -10  5
  */
 public class SortedListToBST$M {
     public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 
     public static class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) { val = x; }
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 
     private static TreeNode sortedListToBST(ListNode head) {
@@ -42,27 +46,31 @@ public class SortedListToBST$M {
             return null;
         ListNode mid = findMid(head);
         TreeNode root = new TreeNode(mid.val);
+        if (head == mid)
+            return root;
         root.right = sortedListToBST(mid.next);
-        mid.next = null;
         root.left = sortedListToBST(head);
         return root;
     }
 
-    private static ListNode findMid(ListNode head){
+    private static ListNode findMid(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        while (fast.next != null && fast.next.next !=null){
-            if (fast.next != null && fast.next.next != null)
-                fast = fast.next.next;
-                slow = slow.next;
+        ListNode prevPtr = null;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            prevPtr = slow;
+            slow = slow.next;
         }
+        if (prevPtr != null)
+            prevPtr.next = null;
         return slow;
     }
 
-    private static void print(TreeNode node){
-        System.out.println(node.left);
+    private static void print(TreeNode node) {
+        print(node.left);
         System.out.println(node.val);
-        System.out.println(node.right);
+        print(node.right);
     }
 
     public static void main(String[] args) {
