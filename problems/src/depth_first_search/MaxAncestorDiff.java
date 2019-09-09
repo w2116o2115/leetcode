11d6055d6253a4d5b8dd7a1130d4c8c1d20d3c45
zhangwei112 <zhangwei112@jd.com>
2019-09-09 19:35:41 +0800

    呵呵

---
+++ problems/src/depth_first_search/MaxAncestorDiff.java
@@ -0,0 +1,70 @@
+package depth_first_search;
+
+import java.util.List;
+
+/**
+ * 1026. 节点与其祖先之间的最大差值
+ *
+ * 给定二叉树的根节点 root，找出存在于不同节点 A 和 B 之间的最大值 V，其中 V = |A.val - B.val|，且 A 是 B 的祖先。
+ *
+ * （如果 A 的任何子节点之一为 B，或者 A 的任何子节点是 B 的祖先，那么我们认为 A 是 B 的祖先）
+ *
+ *  
+ *
+ * 示例：
+ *
+ *
+ *
+ * 输入：[8,3,10,1,6,null,14,null,null,4,7,13]
+ * 输出：7
+ * 解释：
+ * 我们有大量的节点与其祖先的差值，其中一些如下：
+ * |8 - 3| = 5
+ * |3 - 7| = 4
+ * |8 - 1| = 7
+ * |10 - 13| = 3
+ * 在所有可能的差值中，最大值 7 由 |8 - 1| = 7 得出。
+ *
+ */
+public class MaxAncestorDiff {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxAncestorDiff(TreeNode root) {
+        int left = maxAncestorDiff(root.left, root.val, root.val);
+        int right = maxAncestorDiff(root.right, root.val, root.val);
+        return left > right ? left : right;
+    }
+
+    public int maxAncestorDiff(TreeNode root, int max, int min){
+        if(root == null){
+            return 0;
+        }
+        if(root.val > max){
+            max = root.val;
+        }
+        else if(root.val < min){
+            min = root.val;
+        }
+        if(root.left == null && root.right == null){
+            return max - min;
+        }
+        int left = maxAncestorDiff(root.left, max, min);
+        int right = maxAncestorDiff(root.right, max, min);
+        return left > right ? left : right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(17);
+        System.out.println(new MaxAncestorDiff().maxAncestorDiff(root));
+    }
+}
