a5ea39e121527eb241bbe77488a0cb8246413f7d
zhangwei112 <zhangwei112@jd.com>
2019-09-16 12:48:10 +0800

    呵呵

---
+++ problems/src/depth_first_search/CanFinish.java
@@ -0,0 +1,140 @@
+package depth_first_search;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 207. 课程表
+ *
+ * 现在你总共有 n 门课需要选，记为 0 到 n-1。
+ *
+ * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们: [0,1]
+ *
+ * 给定课程总量以及它们的先决条件，判断是否可能完成所有课程的学习？
+ *
+ * 示例 1:
+ *
+ * 输入: 2, [[1,0]]
+ * 输出: true
+ * 解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
+ * 示例 2:
+ *
+ * 输入: 2, [[1,0],[0,1]]
+ * 输出: false
+ * 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
+ * 说明:
+ *
+ * 输入的先决条件是由边缘列表表示的图形，而不是邻接矩阵。详情请参见图的表示法。
+ * 你可以假定输入的先决条件中没有重复的边。
+ * 提示:
+ *
+ * 这个问题相当于查找一个循环是否存在于有向图中。如果存在循环，则不存在拓扑排序，因此不可能选取所有课程进行学习。
+ * 通过 DFS 进行拓扑排序 - 一个关于Coursera的精彩视频教程（21分钟），介绍拓扑排序的基本概念。
+ * 拓扑排序也可以通过 BFS 完成。
+ *
+ * 学习:
+ * 在图论中，拓扑排序（Topological Sorting）是一个有向无环图（DAG, Directed Acyclic Graph）的所有顶点的线性序列。且该序列必须满足下面两个条件：
+ *
+ * 每个顶点出现且只出现一次。
+ * 若存在一条从顶点 A 到顶点 B 的路径，那么在序列中顶点 A 出现在顶点 B 的前面。
+ *
+ * 拓扑排序呢？这里说一种比较常用的方法：
+ *
+ * 从 DAG 图中选择一个 没有前驱（即入度为0）的顶点并输出。
+ * 从图中删除该顶点和所有以它为起点的有向边。
+ * 重复 1 和 2 直到当前的 DAG 图为空或当前图中不存在无前驱的顶点为止。后一种情况说明有向图中必然存在环。
+ *
+ * 拓扑排序
+ * 这里要使用逆邻接表。其实就是检测这个有向图中有没有环，只要存在环，这些课程就不能按要求学完。
+ *
+ * 具体方法是：
+ *
+ * 第 1 步：构建逆邻接表；
+ *
+ * 第 2 步：递归处理每一个还没有被访问的结点，具体做法很简单：对于一个结点来说，先输出指向它的所有顶点，再输出自己。
+ *
+ * 第 3 步：如果这个顶点还没有被遍历过，就递归遍历它，把所有指向它的结点都输出了，再输出自己。注意：当访问一个结点的时候，
+ * 应当先递归访问它的前驱结点，直至前驱结点没有前驱结点为止。
+ */
+public class CanFinish {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses <= 0) {
+            return false;
+        }
+        int plen = prerequisites.length;
+        if (plen == 0) {
+            return true;
+        }
+        int[] marked = new int[numCourses];
+
+        // 初始化有向图 begin
+        HashSet<Integer>[] graph = new HashSet[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new HashSet<>();
+        }
+        // 初始化有向图 end
+        // 有向图的 key 是前驱结点，value 是后继结点的集合
+        for (int[] p : prerequisites) {
+            graph[p[0]].add(p[1]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, graph, marked)) {
+                // 注意方法的语义，如果图中存在环，表示课程任务不能完成，应该返回 false
+                return false;
+            }
+        }
+        // 在遍历的过程中，一直 dfs 都没有遇到已经重复访问的结点，就表示有向图中没有环
+        // 所有课程任务可以完成，应该返回 true
+        return true;
+    }
+
+    /**
+     * 注意这个 dfs 方法的语义
+     * @param i      当前访问的课程结点
+     * @param graph
+     * @param marked 如果 == 1 表示正在访问中，如果 == 2 表示已经访问完了
+     * @return true 表示图中存在环，false 表示访问过了，不用再访问了
+     */
+    private boolean dfs(int i,
+                        HashSet<Integer>[] graph,
+                        int[] marked) {
+        // 如果访问过了，就不用再访问了
+        if (marked[i] == 1) {
+            // 从正在访问中，到正在访问中，表示遇到了环
+            return true;
+        }
+
+        if (marked[i] == 2) {
+            // 表示在访问的过程中没有遇到环，这个节点访问过了
+            return false;
+        }
+        // 走到这里，是因为初始化呢，此时 marked[i] == 0
+        // 表示正在访问中
+        marked[i] = 1;
+        // 后继结点的集合
+        HashSet<Integer> successorNodes = graph[i];
+
+        for (Integer successor : successorNodes) {
+            if (dfs(successor, graph, marked)) {
+                // 层层递归返回 true ，表示图中存在环
+                return true;
+            }
+        }
+        // i 的所有后继结点都访问完了，都没有存在环，则这个结点就可以被标记为已经访问结束
+        // 状态设置为 2
+        marked[i] = 2;
+        // false 表示图中不存在环
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int numCourses = 2;
+        int[][] prerequisites = new int[numCourses][numCourses];
+        prerequisites[0] = new int[]{1,0};
+        prerequisites[1] = new int[]{0,1};
+        System.out.println(new CanFinish().canFinish(numCourses,prerequisites));
+    }
+}
