37001a9a5955f9a125bddaadcb42832237749960
zhangwei112 <zhangwei112@jd.com>
2019-09-24 09:47:29 +0800

    呵呵

---
+++ problems/src/linked_list/SortList.java
@@ -0,0 +1,79 @@
+package linked_list;
+
+/**
+ * 148. 排序链表
+ *
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ *
+ * 示例 1:
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2:
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ */
+public class SortList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+
+    private ListNode getMid(ListNode head){
+        if (head.next == null)
+            return head;
+        ListNode pre = null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast!=null && fast.next != null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (pre != null){
+            pre.next = null;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(3);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode root = new SortList().sortList(head);
+        System.out.println(1);
+    }
+}
