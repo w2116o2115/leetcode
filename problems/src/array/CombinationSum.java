29faaf883863669c3b42079ab853102b357e718d
zhangwei112 <zhangwei112@jd.com>
2019-12-06 18:01:55 +0800

    呵呵

--- problems/src/array/CombinationSum.java
+++ problems/src/array/CombinationSum.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,6 +35,29 @@ import java.util.List;
  */
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, res, 0, new ArrayList<Integer>());
+        return res;
+    }
+
+    private void backtrack(int[] candidates, int target, List<List<Integer>> res, int i, ArrayList<Integer> tmp_list) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.add(new ArrayList<>(tmp_list));
+            return;
+        }
+        for (int start = i; start < candidates.length; start++) {
+            if (target < candidates[start]) break;
+            tmp_list.add(candidates[start]);
+            backtrack(candidates, target - candidates[start], res, start, tmp_list);
+            tmp_list.remove(tmp_list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = new int[]{2,3,5};
+
+        System.out.println(new CombinationSum().combinationSum(candidates,8));
     }
 }
---
+++ problems/src/breadth_first_search/Flatten.java
@@ -0,0 +1,95 @@
+package breadth_first_search;
+
+/**
+ * 114. 二叉树展开为链表
+ *
+ * 给定一个二叉树，原地将它展开为链表。
+ *
+ * 例如，给定二叉树
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * 将其展开为：
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ *
+ *  将左子树  插到又子树的地方
+ *  //将 1 的左子树插入到右子树的地方
+ *     1
+ *      \
+ *       2         5
+ *      / \         \
+ *     3   4         6
+ * //将原来的右子树接到左子树的最右边节点
+ *     1
+ *      \
+ *       2
+ *      / \
+ *     3   4
+ *          \
+ *           5
+ *            \
+ *             6
+ *
+ *  //将 2 的左子树插入到右子树的地方
+ *     1
+ *      \
+ *       2
+ *        \
+ *         3       4
+ *                  \
+ *                   5
+ *                    \
+ *                     6
+ */
+public class Flatten {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private TreeNode pre = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        System.out.println(root.val);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(4);
+        TreeNode t3 = new TreeNode(6);
+        root.left.left = t1;
+        root.left.right = t2;
+        root.right.right = t3;
+
+        new Flatten().flatten(root);
+    }
+}
