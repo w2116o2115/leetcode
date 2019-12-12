3faad2fde8abddb7058a8debeb3b2683d6e5fab8
zhangwei112 <zhangwei112@jd.com>
2019-12-12 18:18:03 +0800

    呵呵

---
+++ problems/src/breadth_first_search/LevelOrder.java
@@ -0,0 +1,68 @@
+package breadth_first_search;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层次遍历
+ *
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ */
+public class LevelOrder {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int count = queue.size();
+            while (count --> 0){
+                TreeNode node = queue.poll();
+                if (node.left!=null) queue.add(node.left);
+                if (node.right!=null) queue.add(node.right);
+                list.add(node.val);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(new LevelOrder().levelOrder(root));
+    }
+}
