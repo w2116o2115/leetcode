3fab3de724707da80c633ca154f267b6e970854d
zhangwei112 <zhangwei112@jd.com>
2019-12-27 11:04:08 +0800

    呵呵

---
+++ problems/src/top100/FindDisappearedNumbers.java
@@ -0,0 +1,45 @@
+package top100;
+
+import dynamic_programming.zeroOrOneknapsack.FindTargetSumWays;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 448. 找到所有数组中消失的数字
+ *
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ *
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ *
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * 输出:
+ * [5,6]
+ */
+public class FindDisappearedNumbers {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int num:nums){
+            set.add(num);
+        }
+        for (int i=1;i<=nums.length;i++){
+            if (!set.contains(i)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindDisappearedNumbers().findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
+    }
+}
