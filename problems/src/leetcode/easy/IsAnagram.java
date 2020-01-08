7077a56123609f9dd46961a152457748d2090358
zhangwei112 <zhangwei112@jd.com>
2020-01-08 18:49:00 +0800

    呵呵

---
+++ problems/src/leetcode/easy/IsAnagram.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 242. 有效的字母异位词
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 说明:
+ * 你可以假设字符串只包含小写字母。
+ *
+ * 进阶:
+ * 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ */
+public class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        char[] cs = s.toCharArray();
+        char[] ts = t.toCharArray();
+        Arrays.sort(cs);
+        Arrays.sort(ts);
+        String sc = String.valueOf(cs);
+        String st = String.valueOf(ts);
+        return sc.equals(st);
+    }
+}
