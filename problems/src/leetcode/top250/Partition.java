8110b447d59cdb50cdcc3fd31469e42188a60e26
zhangwei112 <zhangwei112@jd.com>
2020-01-14 09:37:25 +0800

    呵呵

---
+++ problems/src/leetcode/top250/Partition.java
@@ -0,0 +1,47 @@
+package leetcode.top250;
+
+/**
+ * 86. 分隔链表
+ *
+ * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ *
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ *
+ * 示例:
+ *
+ * 输入: head = 1->4->3->2->5->2, x = 3
+ * 输出: 1->2->2->4->3->5
+ *
+ * 头插法，生成两个链表
+ */
+public class Partition {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+        ListNode p1 = dummy1;
+        ListNode p2 = dummy2;
+        while (head != null) {
+            if (head.val < x) {
+                p1.next = head;
+                p1 = p1.next;
+            } else {
+                p2.next = head;
+                p2 = p2.next;
+            }
+            head = head.next;
+        }
+        p1.next = dummy2.next;
+        p2.next = null;
+        return dummy1.next;
+    }
+
+}
