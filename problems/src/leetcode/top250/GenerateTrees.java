6da57e061b8b4399d3c3e6b04a3ab5e8434e7c81
zhangwei112 <zhangwei112@jd.com>
2020-01-14 15:14:58 +0800

    呵呵

---
+++ problems/src/leetcode/top250/GenerateTrees.java
@@ -0,0 +1,76 @@
+package leetcode.top250;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 95. 不同的二叉搜索树 II
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出:
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释:
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ * 思路：
+ * 使用分治+递归的方法建树，[1,n]中取k(1<=k<=n)为根节点，则他的左子树必定为[1,k-1]，
+ * 右子树为[k+1,n]，所以递归调用建树的方法，最后把他所有的左右子树整合在一起
+ */
+public class GenerateTrees {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public List<TreeNode> generateTrees(int n) {
+        if(n == 0) return new ArrayList<>();
+        return generateTreeHelper(1,n);
+    }
+    public List<TreeNode> generateTreeHelper(int i, int j){
+        List<TreeNode> res = new ArrayList<>();
+        //i>j返回一个空的子树
+        if(i > j) {
+            res.add(null);
+            return res;
+        }
+        //i==j返回一个结点的子树
+        if(i == j){
+            res.add(new TreeNode(i));
+            return res;
+        }
+        //分治+递归建树
+        for(int k = i; k <= j; k++){
+            List<TreeNode> left = generateTreeHelper(i,k-1);
+            List<TreeNode> right = generateTreeHelper(k+1,j);
+            for(TreeNode tmpR: right){
+                for(TreeNode tmpL: left){
+                    TreeNode tn = new TreeNode(k);
+                    tn.left = tmpL;
+                    tn.right = tmpR;
+                    res.add(tn);
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ problems/src/leetcode/top250/RestoreIpAddresses.java
@@ -0,0 +1,44 @@
+package leetcode.top250;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 93. 复原IP地址
+ * 给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+ *
+ * 示例:
+ *
+ * 输入: "25525511135"
+ * 输出: ["255.255.11.135", "255.255.111.35"]
+ */
+public class RestoreIpAddresses {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ret = new ArrayList<>();
+
+        StringBuilder ip = new StringBuilder();
+
+        for(int a = 1 ; a < 4 ; ++ a)
+            for(int b = 1 ; b < 4 ; ++ b)
+                for(int c = 1 ; c < 4 ; ++ c)
+                    for(int d = 1 ; d < 4 ; ++ d) {
+                        if(a + b + c + d == s.length() ) {
+                            int n1 = Integer.parseInt(s.substring(0, a));
+                            int n2 = Integer.parseInt(s.substring(a, a+b));
+                            int n3 = Integer.parseInt(s.substring(a+b, a+b+c));
+                            int n4 = Integer.parseInt(s.substring(a+b+c));
+                            if(n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255) {
+                                ip.append(n1).append('.').append(n2)
+                                        .append('.').append(n3).append('.').append(n4);
+                                if(ip.length() == s.length() + 3) ret.add(ip.toString());
+                                ip.delete(0, ip.length());
+                            }
+                        }
+                    }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RestoreIpAddresses().restoreIpAddresses("0000"));
+    }
+}
