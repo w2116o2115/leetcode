9eb891fabf338d885f79644132b4641c5d2fbb14
zhangwei112 <zhangwei112@jd.com>
2020-01-16 10:20:13 +0800

    呵呵

---
+++ problems/src/sql/182. 查找重复的电子邮箱.sql
@@ -0,0 +1,26 @@
+-- 编写一个 SQL 查询，查找 Person 表中所有重复的电子邮箱。
+--
+-- 示例：
+--
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- 根据以上输入，你的查询应返回以下结果：
+--
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+
+select Email from
+(
+  select Email, count(Email) as num
+  from Person
+  group by Email
+) as statistic
+where num > 1
---
+++ problems/src/sql/185. 部门工资前三高的所有员工.sql
@@ -0,0 +1,64 @@
+-- Employee 表包含所有员工信息，每个员工有其对应的工号 Id，姓名 Name，工资 Salary 和部门编号 DepartmentId 。
+--
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 85000  | 1            |
+-- | 2  | Henry | 80000  | 2            |
+-- | 3  | Sam   | 60000  | 2            |
+-- | 4  | Max   | 90000  | 1            |
+-- | 5  | Janet | 69000  | 1            |
+-- | 6  | Randy | 85000  | 1            |
+-- | 7  | Will  | 70000  | 1            |
+-- +----+-------+--------+--------------+
+-- Department 表包含公司所有部门的信息。
+--
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+-- 编写一个 SQL 查询，找出每个部门获得前三高工资的所有员工。例如，根据上述给定的表，查询结果应返回：
+--
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | IT         | Randy    | 85000  |
+-- | IT         | Joe      | 85000  |
+-- | IT         | Will     | 70000  |
+-- | Sales      | Henry    | 80000  |
+-- | Sales      | Sam      | 60000  |
+-- +------------+----------+--------+
+
+-- 方法：使用 JOIN 和子查询
+
+-- 公司里前 3 高的薪水意味着有不超过 3 个工资比这些值大。
+
+select e1.Name as 'Employee', e1.Salary
+from Employee e1
+where 3 >
+(
+    select count(distinct e2.Salary)
+    from Employee e2
+    where e2.Salary > e1.Salary
+)
+;
+
+SELECT
+    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+FROM
+    Employee e1
+        JOIN
+    Department d ON e1.DepartmentId = d.Id
+WHERE
+    3 > (SELECT
+            COUNT(DISTINCT e2.Salary)
+        FROM
+            Employee e2
+        WHERE
+            e2.Salary > e1.Salary
+                AND e1.DepartmentId = e2.DepartmentId
+        )
+;
