5135da4c8af6c30e976ec3fd3b4df62bc6ec104f
zhangwei112 <zhangwei112@jd.com>
2020-04-23 10:39:54 +0800

    呵呵

--- problems/src/array/NextPermutation.java
+++ problems/src/array/NextPermutation.java
@@ -1,10 +1,7 @@
-package leetcode.top100;
-
-import java.util.Arrays;
+package array;
 
 /**
  * 31. 下一个排列
- *
  * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
  *
  * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
@@ -16,51 +13,48 @@ import java.util.Arrays;
  * 3,2,1 → 1,2,3
  * 1,1,5 → 1,5,1
  *
- * 思路
+ * 思路：1.从左到右找出第二大的数的索引，如果没有直接反转 nums[k] < num[k+1]
+ *      2. 找出比k更大的数字索引然后交换
+ *      3. 反转 k+1以后的数字，保证第二小
  *
- 先找出最大的索引 k 满足 nums[k] < nums[k+1]，如果不存在，就翻转整个数组；
- 再找出另一个最大索引 l 满足 nums[l] > nums[k]；
- 交换 nums[l] 和 nums[k]；
- 最后翻转 nums[k+1:]。
  */
 public class NextPermutation {
     public void nextPermutation(int[] nums) {
-        if (nums == null || nums.length == 0) return;
         int firstIndex = -1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
+        for (int i=nums.length-2;i>=0;i--){
+            if (nums[i] < nums[i+1]){
                 firstIndex = i;
                 break;
             }
         }
-        if (firstIndex == -1) {
-            reverse(nums, 0, nums.length - 1);
+        if (firstIndex == -1){
+            rev(nums,0,nums.length-1);
             return;
         }
+
         int secondIndex = -1;
-        for (int i = nums.length - 1; i >= 0; i--) {
+        for (int i=nums.length-1;i>=0;i--){
             if (nums[i] > nums[firstIndex]) {
                 secondIndex = i;
                 break;
             }
         }
-        swap(nums, firstIndex, secondIndex);
-        reverse(nums, firstIndex + 1, nums.length - 1);
+        sweap(nums,firstIndex,secondIndex);
+        rev(nums,firstIndex+1,nums.length-1);
         return;
-
     }
 
-    private void reverse(int[] nums, int i, int j) {
-        while (i < j) {
-            swap(nums, i++, j--);
+    private void rev(int[] nums,int start,int end){
+        while (start<end){
+            sweap(nums,start++,end--);
         }
     }
 
-    private void swap(int[] nums, int i, int i1) {
-        int tmp = nums[i];
-        nums[i] = nums[i1];
-        nums[i1] = tmp;
-    }
+    private void sweap(int[] nums,int a,int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+     }
 
     public static void main(String[] args) {
         new NextPermutation().nextPermutation(new int[]{1,3,2});
