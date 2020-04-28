a5667a6430f3d8470ba96f2cc336888c53f58cfb
zhangwei112 <zhangwei112@jd.com>
2020-04-28 11:40:01 +0800

    呵呵

--- problems/src/array/Merge.java
+++ problems/src/array/Merge.java
@@ -25,18 +25,18 @@ import java.util.List;
 public class Merge {
     public int[][] merge(int[][] intervals) {
         List<int[]> res = new ArrayList<>();
-        if (intervals.length == 0 || intervals == null) return res.toArray(new int[0][]);
+        if (null==intervals || intervals.length == 0)
+            return res.toArray(new int[0][]);
         Arrays.sort(intervals,Comparator.comparingInt(a->a[0]));
-        int i=0;
-        while (i<intervals.length){
-            int left = intervals[i][0];
-            int right = intervals[i][1];
-            while (i<intervals.length && intervals[i+1][0] <= right){
-                i++;
-                right = Math.max(right,intervals[i][1]);
+        int index=0;
+        while (index < intervals.length){
+            int left = intervals[index][0];
+            int right = intervals[index][1];
+            while (index<intervals.length && intervals[index+1][0] <= right){
+                index++;
+                right = Math.max(right,intervals[index][1]);
             }
-            res.add(new int[]{left,right});
-            i++;
+            index++;
         }
         return res.toArray(new int[0][]);
     }
