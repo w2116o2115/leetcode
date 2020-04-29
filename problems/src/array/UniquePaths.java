3bef3f8600b17744e5cb032af227fb91899559c5
zhangwei112 <zhangwei112@jd.com>
2020-04-29 10:20:58 +0800

    呵呵

---
+++ problems/src/array/UniquePaths.java
@@ -0,0 +1,27 @@
+package array;
+
+/**
+ * 62. 不同路径
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ *
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ *
+ * 问总共有多少条不同的路径？
+ *
+ * 思路: 动态规划 dp[m][n] = dp[m-][n] + dp[m][n-1]
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if (i==0 || j==0){
+                    dp[i][j] = 1;
+                }else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
