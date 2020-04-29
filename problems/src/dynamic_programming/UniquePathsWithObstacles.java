b672eefee11b017f5a75abcd2f154f8a63e96b1a
zhangwei112 <zhangwei112@jd.com>
2020-04-29 10:54:43 +0800

    呵呵

--- problems/src/dynamic_programming/UniquePathsWithObstacles.java
+++ problems/src/dynamic_programming/UniquePathsWithObstacles.java
@@ -14,14 +14,14 @@ public class UniquePathsWithObstacles {
             return 0;
         }
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-
+        dp[0][0] = 1;
         for (int i = 1; i < R; i++) {
-            dp[i][0] = (dp[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
+            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
         }
 
         // Filling the values for the first row
         for (int i = 1; i < C; i++) {
-            dp[0][i] = (dp[0][i] == 0 && dp[0][i - 1] == 1) ? 1 : 0;
+            dp[0][i] = (obstacleGrid[0][i] == 0 && dp[0][i - 1] == 1) ? 1 : 0;
         }
 
         for (int i = 1; i < R; i++) {
