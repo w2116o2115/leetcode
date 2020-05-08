67c8f3d9996f4ed229104b15e46c25698b4790d1
zhangwei112 <zhangwei112@jd.com>
2020-05-08 10:37:51 +0800

    呵呵

--- problems/src/string/LengthOfLongestSubstring.java
+++ problems/src/string/LengthOfLongestSubstring.java
@@ -24,40 +24,31 @@ import java.util.Arrays;
  * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
  *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
  * 子串  问题就是  动态规划问题
+ *
+ * 思路： 滑动窗口
  */
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-        if ("".equals(s))
-            return 0;
-        if (s.length() == 1)
-            return 1;
-        char[] c = s.toCharArray();
-        int[] dp = new int[s.length()];
-        Arrays.fill(dp,1);
-        int res = 0;
-        int start = 0;
-        boolean flag = false;
-        for (int i=1;i<c.length;i++){
-            for (int j=start;j<i;j++) {
-                if (c[i] == c[j]) {
-                    flag = !flag;
-                    start = j+1;
-                    break;
-                }
-            }
-            if (flag){
-                flag = !flag;
-                dp[start] = 1;
-                i = start;
+        if (s == null || s.length() == 0) return 0;
+        int res = Integer.MIN_VALUE;
+        int left = 0;
+        int right = 0;
+        while (right < s.length()){
+            if (right > 0 && !s.substring(left, right).contains(String.valueOf(s.charAt(right)))){
+                res = res>right - left + 1?res:right - left + 1;
             }else {
-                dp[i] = dp[i-1] + 1;
+                left = !s.substring(left, right).contains(String.valueOf(s.charAt(right))) ?right:s.substring(left, right).indexOf(String.valueOf(s.charAt(right)))+1;
+                s = s.substring(left);
+                left = 0;right =0;
             }
-            res = Math.max(res,dp[i]);
+            right++;
         }
+        if (res == Integer.MIN_VALUE)
+            return 1;
         return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("dvdf"));
+        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("pwwkew"));
     }
 }
