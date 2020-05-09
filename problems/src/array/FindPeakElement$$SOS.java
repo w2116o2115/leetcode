946fee2bd01bbcd288aec0cbcee3ebce587d97a2
zhangwei112 <zhangwei112@jd.com>
2020-05-09 16:18:21 +0800

    呵呵

--- problems/src/array/FindPeakElement$$SOS.java
+++ problems/src/array/FindPeakElement$$SOS.java
@@ -30,7 +30,7 @@ public class FindPeakElement$$SOS {
         int left=0;
         int right=nums.length-1;
         while(left<right){
-            int mid=left+(right-left)/2;
+            int mid=left+right >> 1;
             if(nums[mid]>nums[mid+1]){
                 right=mid;
             }
