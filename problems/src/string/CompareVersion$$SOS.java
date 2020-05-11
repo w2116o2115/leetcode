0404b4a69316a8883343317525fb8fa95fed0900
zhangwei112 <zhangwei112@jd.com>
2020-05-11 09:40:09 +0800

    呵呵

---
+++ problems/src/string/CompareVersion$$SOS.java
@@ -0,0 +1,73 @@
+package string;
+
+/**
+ * 165. 比较版本号
+ * 比较两个版本号 version1 和 version2。
+ * 如果 version1 > version2 返回 1，如果 version1 < version2 返回 -1， 除此之外返回 0。
+ *
+ * 你可以假设版本字符串非空，并且只包含数字和 . 字符。
+ *
+ *  . 字符不代表小数点，而是用于分隔数字序列。
+ *
+ * 例如，2.5 不是“两个半”，也不是“差一半到三”，而是第二版中的第五个小版本。
+ *
+ * 你可以假设版本号的每一级的默认修订版号为 0。例如，版本号 3.4 的第一级（大版本）和第二级（小版本）修订号分别为 3 和 4。其第三级和第四级修订号均为 0。
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: version1 = "0.1", version2 = "1.1"
+ * 输出: -1
+ * 示例 2:
+ *
+ * 输入: version1 = "1.0.1", version2 = "1"
+ * 输出: 1
+ * 示例 3:
+ *
+ * 输入: version1 = "7.5.2.4", version2 = "7.5.3"
+ * 输出: -1
+ * 示例 4：
+ *
+ * 输入：version1 = "1.01", version2 = "1.001"
+ * 输出：0
+ * 解释：忽略前导零，“01” 和 “001” 表示相同的数字 “1”。
+ * 示例 5：
+ *
+ * 输入：version1 = "1.0", version2 = "1.0.0"
+ * 输出：0
+ * 解释：version1 没有第三级修订号，这意味着它的第三级修订号默认为 “0”。
+ *
+ * 思路很简单，按照「点」对版本号进行切割，然后依次比较每个数字即可。
+ */
+public class CompareVersion$$SOS {
+    public int compareVersion(String version1, String version2) {
+        String[] nums1 = version1.split("\\.");
+        String[] nums2 = version2.split("\\.");
+        int i = 0, j = 0;
+        while (i < nums1.length || j < nums2.length) {
+            //这个技巧经常用到，当一个已经遍历结束的话，我们将其赋值为 0
+            String num1 = i < nums1.length ? nums1[i] : "0";
+            String num2 = j < nums2.length ? nums2[j] : "0";
+            int res = compare(num1, num2);
+            if (res == 0) {
+                i++;
+                j++;
+            } else {
+                return res;
+            }
+        }
+        return 0;
+    }
+
+    private int compare(String num1, String num2) {
+        int n1 = Integer.parseInt(num1);
+        int n2 = Integer.parseInt(num2);
+        if (n1 > n2) {
+            return 1;
+        } else if (n1 < n2) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
