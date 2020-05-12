bbc09f0e5d3378498a0014c02652c6ab8a945cb6
zhangwei112 <zhangwei112@jd.com>
2020-05-12 09:56:01 +0800

    呵呵

---
+++ problems/src/string/NextGreaterElement.java
@@ -0,0 +1,59 @@
+package string;
+
+/**
+ * 556. 下一个更大元素 III
+ * 给定一个32位正整数 n，你需要找到最小的32位整数，其与 n 中存在的位数完全相同，并且其值大于n。如果不存在这样的32位整数，则返回-1。
+ *
+ * 示例 1:
+ *
+ * 输入: 12
+ * 输出: 21
+ * 示例 2:
+ *
+ * 输入: 21
+ * 输出: -1
+ *
+ * [9, 5, 4, 3, 1]
+ * 思路： 首先从右边开始找到a[i-1] < a[i] 这时可以认为右边的数据是升序的
+ *        然后从右边的数据中找到 a[j] > a[i] 的数据，然后进行交换就得到了 最小的32位整数
+ *        最后反转 i+1后边的数据
+ */
+public class NextGreaterElement {
+    public int nextGreaterElement(int n) {
+        char[] a = ("" + n).toCharArray();
+        int i = a.length - 2;
+        while (i >= 0 && a[i + 1] <= a[i]) {
+            i--;
+        }
+        if (i < 0)
+            return -1;
+        int j = a.length - 1;
+        while (j >= 0 && a[j] <= a[i]) {
+            j--;
+        }
+        swap(a, i, j);
+        reverse(a, i + 1);
+        try {
+            return Integer.parseInt(new String(a));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    private void reverse(char[] a, int start) {
+        int i = start, j = a.length - 1;
+        while (i < j) {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+    }
+    private void swap(char[] a, int i, int j) {
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new NextGreaterElement().nextGreaterElement(1487531));
+    }
+}
