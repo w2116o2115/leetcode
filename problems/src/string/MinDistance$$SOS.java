a3ba3a7b3e671b0f88c4100e7715e1df3230d9ec
zhangwei112 <zhangwei112@jd.com>
2020-05-13 13:41:42 +0800

    呵呵

---
+++ problems/src/string/MinDistance$$SOS.java
@@ -0,0 +1,27 @@
+package string;
+
+/**
+ * 583. 两个字符串的删除操作
+ * 给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ *
+ * 示例 1:
+ *
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ *
+ * 思路： 最长
+ */
+public class MinDistance$$SOS {
+    public int minDistance(String s1, String s2) {
+        return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length());
+    }
+    public int lcs(String s1, String s2, int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (s1.charAt(m - 1) == s2.charAt(n - 1))
+            return 1 + lcs(s1, s2, m - 1, n - 1);
+        else
+            return Math.max(lcs(s1, s2, m, n - 1), lcs(s1, s2, m - 1, n));
+    }
+}
