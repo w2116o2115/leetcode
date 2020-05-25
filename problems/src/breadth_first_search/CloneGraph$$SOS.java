7a2bff814fdd3dbef2e088f933660319876e92c9
zhangwei112 <zhangwei112@jd.com>
2020-05-25 10:08:34 +0800

    呵呵

---
+++ problems/src/breadth_first_search/CloneGraph$$SOS.java
@@ -0,0 +1,65 @@
+package breadth_first_search;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 133. 克隆图
+ * 给你无向 连通 图中一个节点的引用，请你返回该图的 深拷贝（克隆）。
+ *
+ * 图中的每个节点都包含它的值 val（int） 和其邻居的列表（list[Node]）。
+ *
+ * class Node {
+ *     public int val;
+ *     public List<Node> neighbors;
+ * }
+ *
+ * 解题思路:
+ * 这道题就是遍历整个图，所以遍历时候要记录已经访问点，我们用一个字典记录。
+ *
+ * 所以，遍历方法就有两种。
+ *
+ * 思路一:DFS (深度遍历)
+ *
+ * 思路二:BFS (广度遍历)
+ *
+ * !!! 大家重点掌握，后面图遍历都和这个有关系！
+ */
+public class CloneGraph$$SOS {
+    class  Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> lookup = new HashMap<>();
+        return dfs(node, lookup);
+    }
+
+    private Node dfs(Node node, Map<Node,Node> lookup) {
+        if (node == null) return null;
+        if (lookup.containsKey(node)) return lookup.get(node);
+        Node clone = new Node(node.val, new ArrayList<>());
+        lookup.put(node, clone);
+        for (Node n : node.neighbors)clone.neighbors.add(dfs(n,lookup));
+        return clone;
+    }
+}
