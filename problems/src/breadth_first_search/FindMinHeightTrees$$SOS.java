2a2caca9ca9fdf725b944d6b7ed01c0e475cf02a
zhangwei112 <zhangwei112@jd.com>
2020-05-27 13:34:07 +0800

    呵呵

---
+++ problems/src/breadth_first_search/FindMinHeightTrees$$SOS.java
@@ -0,0 +1,88 @@
+package breadth_first_search;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 310. 最小高度树
+ * 对于一个具有树特征的无向图，我们可选择任何一个节点作为根。图因此可以成为树，在所有可能的树中，具有最小高度的树被称为最小高度树。
+ * 给出这样的一个图，写出一个函数找到所有的最小高度树并返回他们的根节点。
+ *
+ * 格式
+ *
+ * 该图包含 n 个节点，标记为 0 到 n - 1。给定数字 n 和一个无向边 edges 列表（每一个边都是一对标签）。
+ * 你可以假设没有重复的边会出现在 edges 中。由于所有的边都是无向边， [0, 1]和 [1, 0] 是相同的，因此不会同时出现在 edges 里。
+ *
+ * 示例 1:
+ * 输入: n = 4, edges = [[1, 0], [1, 2], [1, 3]]
+ *
+ *         0
+ *         |
+ *         1
+ *        / \
+ *       2   3
+ *
+ * 输出: [1]
+ * 示例 2:
+ *
+ * 输入: n = 6, edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
+ *
+ *      0  1  2
+ *       \ | /
+ *         3
+ *         |
+ *         4
+ *         |
+ *         5
+ *
+ * 输出: [3, 4]
+ */
+public class FindMinHeightTrees$$SOS {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> ans = new ArrayList<>();
+        if (n == 1) {
+            ans.add(0);
+            return ans;
+        }
+        int[] degree = new int[n];
+        List<List<Integer>> map = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            map.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 1) {//度数为1，说明是叶子结点,入队列
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            ans = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                ans.add(cur);
+                List<Integer> nexts = map.get(cur);
+                for (Integer next : nexts) {
+                    degree[next]--;//删除叶子节点后，跟其相邻的节点的度数要减少
+                    if (degree[next] == 1) {
+                        queue.offer(next);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] edges = new int[][]{{0, 3},{1, 3},{2, 3},{4, 3},{5, 4}};
+        System.out.println(new FindMinHeightTrees$$SOS().findMinHeightTrees(6,edges));
+    }
+}
