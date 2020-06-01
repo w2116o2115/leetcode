0c3871209a7dd4e2c3f1ea0c0c678ce753ffd760
zhangwei112 <zhangwei112@jd.com>
2020-06-01 10:43:10 +0800

    呵呵

---
+++ problems/src/breadth_first_search/DistanceK$$SOS.java
@@ -0,0 +1,77 @@
+package breadth_first_search;
+
+import java.util.*;
+
+/**
+ * 863. 二叉树中所有距离为 K 的结点
+ * 给定一个二叉树（具有根结点 root）， 一个目标结点 target ，和一个整数值 K 。
+ *
+ * 返回到目标结点 target 距离为 K 的所有结点的值的列表。 答案可以以任何顺序返回。
+ *
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+ *
+ * 输出：[7,4,1]
+ *
+ * 算法
+ * 对所有节点添加一个指向父节点的引用，之后做深度优先搜索，找到所有距离 target 节点 K 距离的节点
+ */
+public class DistanceK$$SOS {
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    Map<TreeNode, TreeNode> parent;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        parent = new HashMap();
+        dfs(root, null);
+
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(null);
+        queue.add(target);
+        Set<TreeNode> seen = new HashSet();
+        seen.add(target);
+        seen.add(null);
+
+        int dist = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (dist == K) {
+                    List<Integer> ans = new ArrayList();
+                    for (TreeNode n: queue)
+                        ans.add(n.val);
+                    return ans;
+                }
+                queue.offer(null);
+                dist++;
+            } else {
+                if (!seen.contains(node.left)) {
+                    seen.add(node.left);
+                    queue.offer(node.left);
+                }
+                if (!seen.contains(node.right)) {
+                    seen.add(node.right);
+                    queue.offer(node.right);
+                }
+                TreeNode par = parent.get(node);
+                if (!seen.contains(par)) {
+                    seen.add(par);
+                    queue.offer(par);
+                }
+            }
+        }
+
+        return new ArrayList<Integer>();
+    }
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            parent.put(node, par);
+            dfs(node.left, node);
+            dfs(node.right, node);
+        }
+    }
+}
