fefc1eee3f2b182aa4824b21f0735955ee250643
zhangwei112 <zhangwei112@jd.com>
2020-06-02 09:18:33 +0800

    呵呵

---
+++ problems/src/breadth_first_search/MaxDistance$$SOS.java
@@ -0,0 +1,66 @@
+package breadth_first_search;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * 1162. 地图分析
+ * 你现在手里有一份大小为 N x N 的「地图」（网格） grid，上面的每个「区域」（单元格）都用 0 和 1 标记好了。
+ * 其中 0 代表海洋，1 代表陆地，请你找出一个海洋区域，这个海洋区域到离它最近的陆地区域的距离是最大的。
+ *
+ * 我们这里说的距离是「曼哈顿距离」（ Manhattan Distance）：(x0, y0) 和 (x1, y1) 这两个区域之间的距离是 |x0 - x1| + |y0 - y1| 。
+ *
+ * 如果我们的地图上只有陆地或者海洋，请返回 -1。
+ *
+ * 输入：[[1,0,1],[0,0,0],[1,0,1]]
+ * 输出：2
+ * 解释：
+ * 海洋区域 (1, 1) 和所有陆地区域之间的距离都达到最大，最大距离为 2。
+ *
+ * 思路：从各个陆地开始，一圈一圈的遍历海洋，最后遍历到的海洋就是离陆地最远的海洋。
+ */
+public class MaxDistance$$SOS {
+    public int maxDistance(int[][] grid) {
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        int m = grid.length, n = grid[0].length;
+        // 先把所有的陆地都入队。
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    queue.offer(new int[] {i, j});
+                }
+            }
+        }
+
+        // 从各个陆地开始，一圈一圈的遍历海洋，最后遍历到的海洋就是离陆地最远的海洋。
+        boolean hasOcean = false;
+        int[] point = null;
+        while (!queue.isEmpty()) {
+            point = queue.poll();
+            int x = point[0], y = point[1];
+            // 取出队列的元素，将其四周的海洋入队。
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                if (newX < 0 || newX >= m || newY < 0 || newY >= n || grid[newX][newY] != 0) {
+                    continue;
+                }
+                grid[newX][newY] = grid[x][y] + 1; // 这里我直接修改了原数组，因此就不需要额外的数组来标志是否访问
+                hasOcean = true;
+                queue.offer(new int[] {newX, newY});
+            }
+        }
+
+        // 没有陆地或者没有海洋，返回-1。
+        if (point == null || !hasOcean) {
+            return -1;
+        }
+
+        // 返回最后一次遍历到的海洋的距离。
+        return grid[point[0]][point[1]] - 1;
+
+    }
+}
