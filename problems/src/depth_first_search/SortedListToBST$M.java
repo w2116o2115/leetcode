cc10a1367089fac15536f7c9c220f3273ea81fac
zhangwei112 <zhangwei112@jd.com>
2020-06-03 09:27:49 +0800

    呵呵

--- problems/src/depth_first_search/SortedListToBST$M.java
+++ problems/src/depth_first_search/SortedListToBST$M.java
@@ -4,19 +4,16 @@ import java.util.List;
 
 /**
  * 109. 有序链表转换二叉搜索树
- * <p>
  * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
- * <p>
  * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
- * <p>
  * 示例:
- * <p>
+ *
  * 给定的有序链表： [-10, -3, 0, 5, 9],
- * <p>
+ *
  * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
- * <p>
- * 0
- * / \
+ *
+ *    0
+ *  /  \
  * -3   9
  * /   /
  * -10  5
@@ -54,17 +51,21 @@ public class SortedListToBST$M {
     }
 
     private static ListNode findMid(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
         ListNode prevPtr = null;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            prevPtr = slow;
-            slow = slow.next;
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while (fastPtr != null && fastPtr.next != null) {
+            prevPtr = slowPtr;
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
         }
-        if (prevPtr != null)
+
+        if (prevPtr != null) {
             prevPtr.next = null;
-        return slow;
+        }
+
+        return slowPtr;
     }
 
     private static void print(TreeNode node) {
