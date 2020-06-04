30d2829633b09674ec3db69504082328ee8a4ea5
zhangwei112 <zhangwei112@jd.com>
2020-06-04 09:28:25 +0800

    呵呵

--- problems/src/depth_first_search/CanFinish.java
+++ problems/src/depth_first_search/CanFinish.java
@@ -1,9 +1,6 @@
 package depth_first_search;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 207. 课程表
@@ -24,117 +21,101 @@ import java.util.List;
  * 输入: 2, [[1,0],[0,1]]
  * 输出: false
  * 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
- * 说明:
  *
- * 输入的先决条件是由边缘列表表示的图形，而不是邻接矩阵。详情请参见图的表示法。
- * 你可以假定输入的先决条件中没有重复的边。
- * 提示:
+ * 思路 :通拓扑排序思路
  *
- * 这个问题相当于查找一个循环是否存在于有向图中。如果存在循环，则不存在拓扑排序，因此不可能选取所有课程进行学习。
- * 通过 DFS 进行拓扑排序 - 一个关于Coursera的精彩视频教程（21分钟），介绍拓扑排序的基本概念。
- * 拓扑排序也可以通过 BFS 完成。
+ * 不能“跳步”，选你能上的课
+ *  当下只能选 入度为 0 的课，因为它不依赖别的课。假设先选了 0
+ *  这导致 依赖 0 的课的入度减小 —— 3 的入度由 2 变 1
+ *  接着选 1，导致课 3 的入度变 0，课 4 的入度由 2 变 1
+ *  接着选 2，导致课 4 的入度变 0，当前 3 和 4 入度为 0
+ *  继续选 入度为 0 的课
+ *  ……
+ *  直到选不到 入度为 0 的课
  *
- * 学习:
- * 在图论中，拓扑排序（Topological Sorting）是一个有向无环图（DAG, Directed Acyclic Graph）的所有顶点的线性序列。且该序列必须满足下面两个条件：
+ * 这形似 树的BFS
+ *  起初让 入度为 0 的课 入列
+ *  然后 逐个出列，课出列 = 课被选，减小相关课的入度
+ *  判定是否有 入度转 0 的课，继续入列、出列……
+ *  直到没有 入度为 0 的课 可入列……
  *
- * 每个顶点出现且只出现一次。
- * 若存在一条从顶点 A 到顶点 B 的路径，那么在序列中顶点 A 出现在顶点 B 的前面。
+ * BFS 前的准备工作
+ *  我们关心【每门课对应的入度】—— 它要被减，要被监控
+ *  我们关心【课之间的依赖关系】—— 选这门课会减小哪些课的入度
+ *  因此我们需要 合适的数据结构，去存储这些关系
+ *  构建入度数组
+ *  每一门课都有一个动态变化的入度
+ *  课的编号是 0 到 n - 1，让它作为索引，选用 数组 存放入度
+ *  遍历先决条件表，计算每门课的初始入度
  *
- * 拓扑排序呢？这里说一种比较常用的方法：
+ * 构建哈希表
+ *  我们选用 哈希表 即 相邻衔接表 来记录 依赖关系
+ *  map 存键值对：
+ *  键： 课的编号
+ *  值： 依赖它的后续课程
+ *  比如：修完 2 才能修 4 和 5
+ *  2: [4, 5]
+ *  也可以用 邻接矩阵，但它有点大
  *
- * 从 DAG 图中选择一个 没有前驱（即入度为0）的顶点并输出。
- * 从图中删除该顶点和所有以它为起点的有向边。
- * 重复 1 和 2 直到当前的 DAG 图为空或当前图中不存在无前驱的顶点为止。后一种情况说明有向图中必然存在环。
+ * BFS 思路
+ *  queue 队列中始终是 入度为 0 的课 在流动
+ *  选择一门课，就让它 出列，同时 查看哈希表，看它 有哪些后续课
+ *  将这些后续课的 入度 - 1，如果有 减至 0 的，就将它 入列
+ *  不再有新的入度 0 的课入列 时，此时 queue 为空，退出循环
  *
- * 拓扑排序
- * 这里要使用逆邻接表。其实就是检测这个有向图中有没有环，只要存在环，这些课程就不能按要求学完。
- *
- * 具体方法是：
- *
- * 第 1 步：构建逆邻接表；
- *
- * 第 2 步：递归处理每一个还没有被访问的结点，具体做法很简单：对于一个结点来说，先输出指向它的所有顶点，再输出自己。
- *
- * 第 3 步：如果这个顶点还没有被遍历过，就递归遍历它，把所有指向它的结点都输出了，再输出自己。注意：当访问一个结点的时候，
- * 应当先递归访问它的前驱结点，直至前驱结点没有前驱结点为止。
+ * 判断条件
+ *  遍历完所有的课，如果 仍有课的入度不为 0，说明它无法入列，代表选不了它
+ *  遍历入度数组，有课的入度不为 0 ，则返回false，说明完成不了所有课程
+ *  否则，返回true，能找到一种排序，完成所有课程
  */
 public class CanFinish {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (numCourses <= 0) {
-            return false;
-        }
-        int plen = prerequisites.length;
-        if (plen == 0) {
-            return true;
-        }
-        int[] marked = new int[numCourses];
-
-        // 初始化有向图 begin  邻接矩阵  数组 + 链表/hash表
-        HashSet<Integer>[] graph = new HashSet[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            graph[i] = new HashSet<>();
-        }
-        // 初始化有向图 end
-        // 有向图的 key 是前驱结点，value 是后继结点的集合
-        for (int[] p : prerequisites) {
-            graph[p[0]].add(p[1]);
+        if (numCourses <= 0) return false;
+        //建立邻接表
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        //建立入度表
+        int[] degree = new int[numCourses];
+        for (int i=0;i<prerequisites.length;i++){
+            List<Integer> list = map.getOrDefault(prerequisites[i][1],new ArrayList<>());
+            list.add(prerequisites[i][0]);
+            map.put(prerequisites[i][1],list);
+            degree[prerequisites[i][0]]++;
         }
 
-        for (int i = 0; i < numCourses; i++) {
-            if (dfs(i, graph, marked)) {
-                // 注意方法的语义，如果图中存在环，表示课程任务不能完成，应该返回 false
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i=0;i<degree.length;i++){
+            if (degree[i] == 0) queue.offer(i);
+        }
+        bfs(map,degree,queue);
+        for (int i=0;i<degree.length;i++){
+            if (degree[i] != 0)
                 return false;
-            }
         }
-        // 在遍历的过程中，一直 dfs 都没有遇到已经重复访问的结点，就表示有向图中没有环
-        // 所有课程任务可以完成，应该返回 true
         return true;
     }
 
     /**
-     * 注意这个 dfs 方法的语义
-     * @param i      当前访问的课程结点
-     * @param graph
-     * @param marked 如果 == 1 表示正在访问中，如果 == 2 表示已经访问完了
-     * @return true 表示图中存在环，false 表示访问过了，不用再访问了
+     * 广度优先遍历
+     * @param map
+     * @param degree
+     * @param queue
+     * @return
      */
-    private boolean dfs(int i,
-                        HashSet<Integer>[] graph,
-                        int[] marked) {
-        // 如果访问过了，就不用再访问了
-        if (marked[i] == 1) {
-            // 从正在访问中，到正在访问中，表示遇到了环
-            return true;
-        }
-
-        if (marked[i] == 2) {
-            // 表示在访问的过程中没有遇到环，这个节点访问过了
-            return false;
-        }
-        // 走到这里，是因为初始化呢，此时 marked[i] == 0
-        // 表示正在访问中
-        marked[i] = 1; //先定义自己有环   如果最后还是访问到自己就说有环儿
-        // 后继结点的集合
-        HashSet<Integer> successorNodes = graph[i];
-
-        for (Integer successor : successorNodes) {
-            if (dfs(successor, graph, marked)) {
-                // 层层递归返回 true ，表示图中存在环
-                return true;
+    private void bfs(Map<Integer, List<Integer>> map,int[] degree,Queue<Integer> queue) {
+        while (!queue.isEmpty()){
+            List<Integer> list = map.get(queue.poll());
+            if (list==null || list.size() <= 0) continue;
+            for (int next:list){
+                degree[next]--;
+                if (degree[next] == 0)
+                    queue.offer(next);
             }
         }
-        // i 的所有后继结点都访问完了，都没有存在环，则这个结点就可以被标记为已经访问结束
-        // 状态设置为 2
-        marked[i] = 2;
-        // false 表示图中不存在环
-        return false;
     }
 
     public static void main(String[] args) {
         int numCourses = 2;
-        int[][] prerequisites = new int[numCourses][numCourses];
-        prerequisites[0] = new int[]{1,0};
-        prerequisites[1] = new int[]{0,1};
+        int[][] prerequisites = new int[][]{{1,0}};
         System.out.println(new CanFinish().canFinish(numCourses,prerequisites));
     }
 }
