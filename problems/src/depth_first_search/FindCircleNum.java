e4b1141d27d2d6380008062bcc1a85c6ea05e2c5
zhangwei112 <zhangwei112@jd.com>
2020-06-08 09:53:07 +0800

    呵呵

--- problems/src/depth_first_search/FindCircleNum.java
+++ problems/src/depth_first_search/FindCircleNum.java
@@ -3,9 +3,11 @@ package depth_first_search;
 /**
  * 547. 朋友圈
  *
- * 班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+ * 班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，
+ * 那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
  *
- * 给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+ * 给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，
+ * 否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
  *
  * 示例 1:
  *
@@ -31,7 +33,7 @@ public class FindCircleNum {
         int count = 0;
         boolean[] flag = new boolean[length];
         for (int i=0;i<length;i++){
-            if (flag[i] == false){
+            if (!flag[i]){
                 dfs(i,flag,M);//深度优先搜索，访问
                 count++;//朋友圈个数+1
             }
@@ -43,7 +45,7 @@ public class FindCircleNum {
         flag[i] = true;
 
         for(int j = 0;j < M[i].length;j++){
-            if(flag[j] == false && M[i][j] == 1){
+            if(!flag[j] && M[i][j] == 1){
                 dfs(j,flag,M);
             }
         }
