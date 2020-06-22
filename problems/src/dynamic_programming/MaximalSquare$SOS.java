313daf8c44c8a5f19d1a42248cb61e6ed7b7e4f5
zhangwei112 <zhangwei112@jd.com>
2020-06-22 10:11:20 +0800

    呵呵

--- problems/src/dynamic_programming/MaximalSquare$SOS.java
+++ problems/src/dynamic_programming/MaximalSquare$SOS.java
@@ -15,9 +15,10 @@ package dynamic_programming;
  * 1 0 0 1 0
  *
  * 输出: 4
+ * DP[i][j] 表示以格子(i,j)為矩形右下角所能形成的最大矩形邊長
  * 推到公式  if (grid[i][j] == 1) f[i][j] = min(f[i-1][j-1], f[i-1][j], f[i][j-1]) + 1;
  */
-public class MaximalSquare {
+public class MaximalSquare$SOS {
     public int maximalSquare(char[][] matrix) {
         int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
         int[][] dp = new int[rows + 1][cols + 1];
