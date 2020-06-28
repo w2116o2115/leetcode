93162d2c36ac146080ed09f34882f3da948596dd
zhangwei112 <zhangwei112@jd.com>
2020-06-28 17:58:49 +0800

    呵呵

--- problems/src/dynamic_programming/WordBreak.java
+++ problems/src/dynamic_programming/WordBreak.java
@@ -40,11 +40,11 @@ public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
         if (wordDict == null || wordDict.size() == 0) return s.isEmpty();
         int n = s.length();
-        boolean[] dp = new boolean[n + 1];
+        boolean[] dp = new boolean[n+1];
         dp[0] = true;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+        for (int i=1;i<=s.length();i++){
+            for (int j=i-1;j>=0;j--){
+                if (dp[j] && wordDict.contains(s.substring(j,i))){
                     dp[i] = true;
                     break;
                 }
