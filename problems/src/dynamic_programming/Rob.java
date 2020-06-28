f935d271a6b5894d398deaa0b038a2ff422dfe17
zhangwei112 <zhangwei112@jd.com>
2020-06-28 17:11:54 +0800

    呵呵

--- problems/src/dynamic_programming/Rob.java
+++ problems/src/dynamic_programming/Rob.java
@@ -21,27 +21,50 @@ import java.util.Arrays;
  * 输出: 4
  * 解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
  *      偷窃到的最高金额 = 1 + 3 = 4 。
+ *
+ * dp[i] 代表前 ii 个房子在满足条件下的能偷窃到的最高金额。
+ *
+ * 转移方程：
+ * 设： 有 n 个房子，前 n 间能偷窃到的最高金额是 dp[n] ，前 n−1 间能偷窃到的最高金额是 dp[n-1] ，此时向这些房子后加一间房，此房间价值为 num ；
+ * 加一间房间后： 由于不能抢相邻的房子，意味着抢第 n+1 间就不能抢第 n 间；那么前 n+1 间房能偷取到的最高金额 dp[n+1] 一定是以下两种情况的 较大值 ：
+ * 不抢第 n+1 个房间，因此等于前 n 个房子的最高金额，即 dp[n+1] = dp[n]；
+ * 抢第 n+1 个房间，此时不能抢第 n 个房间；因此等于前 n−1 个房子的最高金额加上当前房间价值，即 dp[n+1] = dp[n-1] + num ；
+ *
+ * 细心的我们发现： 难道在前 n 间的最高金额 dp[n] 情况下，第 n 间一定被偷了吗？假设没有被偷，那 n+1 间的最大值应该也可能是 dp[n+1] = dp[n] + num吧？
+ *                 其实这种假设的情况可以被省略，这是因为：
+ * 假设第 n 间没有被偷，那么此时 dp[n] = dp[n-1] ，此时 dp[n+1] = dp[n] + num = dp[n-1] + num ，即可以将 两种情况合并为一种情况 考虑；
+ * 假设第 n 间被偷，那么此时 dp[n+1] = dp[n] + num 不可取 ，因为偷了第 n 间就不能偷第 n+1 间。
+ * 最终的转移方程： dp[n+1] = max(dp[n],dp[n-1]+num)
  */
 public class Rob {
     public int rob(int[] nums) {
-        if (nums.length == 0)
+        if(nums==null || nums.length==0) {
             return 0;
-        if (nums.length == 1)
-            return nums[0];
-        return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length - 1)),
-                myRob(Arrays.copyOfRange(nums, 1, nums.length)));
+        }
+        if(nums.length<=2) {
+            return (nums.length==2) ? Math.max(nums[0],nums[1]) : nums[0];
+        }
+        int n = nums.length;
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        //初始化两个dp数组，dp1是计算的是[1,end],dp2计算的是[0,end-1]
+        dp1[0] = 0;
+        dp1[1] = nums[1];
+        dp2[0] = nums[0];
+        dp2[1] = Math.max(nums[0],nums[1]);
+        //按照【打家劫舍 I】的转移方式执行两遍
+        for(int i=2;i<n;++i) {
+            dp1[i] = Math.max(dp1[i-1],dp1[i-2]+nums[i]);
+        }
+        for(int i=2;i<n-1;++i) {
+            dp2[i] = Math.max(dp2[i-1],dp2[i-2]+nums[i]);
+        }
+        return Math.max(dp1[n-1],dp2[n-2]);
     }
 
-    private int myRob(int[] nums){
-        int temp = 0;
-        int cur = 0;
-        int pre = 0;
 
-        for (int num : nums){
-            temp = cur;
-            cur = Math.max(pre+num, cur);
-            pre = temp;
-        }
-        return cur;
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,3,2};
+        System.out.println(new Rob().rob(nums));
     }
 }
