129a0da2c2e471ee7dfa209bb91ab6cf99da7266
zhangwei112 <zhangwei112@jd.com>
2020-06-28 10:23:06 +0800

    呵呵

--- problems/src/dynamic_programming/unboundedKnapsack/CoinChange.java
+++ problems/src/dynamic_programming/unboundedKnapsack/CoinChange.java
@@ -1,5 +1,7 @@
 package dynamic_programming.unboundedKnapsack;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.Arrays;
 
 /**
@@ -27,16 +29,14 @@ import java.util.Arrays;
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
+        Arrays.fill(dp,Integer.MAX_VALUE);
         dp[0] = 0;
-        for (int i=1;i<=amount;i++){
-            for (int coin : coins){
-                if (i>=coin){
-                    dp[i] = Math.min(dp[i],dp[i-coin]+1);
-                }
+        for (int coin:coins){
+            for (int i=coin;i<=amount;i++){
+                dp[i] = Math.min(dp[i],dp[i-coin]+1);
             }
         }
-        return dp[amount] > amount?-1:dp[amount];
+        return dp[amount];
     }
 
     public static void main(String[] args) {
