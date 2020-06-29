42a3091fd63375628dc76975ceea4a37873562de
zhangwei112 <zhangwei112@jd.com>
2020-06-29 08:52:41 +0800

    呵呵

--- problems/src/array/CanJump$SOS.java
+++ problems/src/array/CanJump$SOS.java
@@ -21,13 +21,18 @@ package array;
  *
  * 贪心算法： 每一步都要跳最大的距离。最后判断是否到达
  */
-public class CanJump {
+public class CanJump$SOS {
     public boolean canJump(int[] nums) {
         int maxPoint = 0;
         for (int i=0;i<nums.length;i++){
             if (i>maxPoint) return false;
-            maxPoint = Math.max(maxPoint,i+nums[i]);
+            maxPoint = Math.max(nums[i],maxPoint);
         }
-        return maxPoint >= nums.length-1;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,1,0,4};
+        System.out.println(new CanJump$SOS().canJump(nums));
     }
 }
