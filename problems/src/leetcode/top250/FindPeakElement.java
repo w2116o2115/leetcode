a60fe01e1d3de25686c9fd5c9ca739c027fe1611
zhangwei112 <zhangwei112@jd.com>
2020-08-04 09:05:31 +0800

    呵呵

--- problems/src/leetcode/top250/FindPeakElement.java
+++ problems/src/leetcode/top250/FindPeakElement.java
@@ -33,10 +33,21 @@ package leetcode.top250;
  */
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1])
-                return i;
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left < right){
+            int mid = (left + right ) >> 1;
+            if (nums[mid] > nums[mid+1]){
+                right = mid;
+            }else {
+                left = mid+1;
+            }
         }
-        return nums.length - 1;
+        return left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindPeakElement().findPeakElement(new int[]{1,2,1,3,5,6,4}));
     }
 }
