48c5f400c0e1fa13bd9f88919a8ff86eba1c94b7
zhangwei112 <zhangwei112@jd.com>
2020-08-04 08:48:01 +0800

    呵呵

--- problems/src/leetcode/top250/DiffWaysToCompute.java
+++ problems/src/leetcode/top250/DiffWaysToCompute.java
@@ -47,27 +47,28 @@ public class DiffWaysToCompute {
     }
 
     public List<Integer> partition(String input) {
-        List<Integer> result = new ArrayList<>();
-        if (!input.contains("+") && !input.contains("-") && !input.contains("*")) {
-            result.add(Integer.valueOf(input));
-            return result;
+        List<Integer> res = new ArrayList<>();
+        if (!input.contains("+") && !input.contains("-") && !input.contains("*")){
+            res.add(Integer.parseInt(input));
+            return res;
         }
-        for(int i = 0; i < input.length(); i++) {
+
+        for (int i=0;i<input.length();i++) {
             if (input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*') {
-                for(Integer left : partition(input.substring(0, i))) {
-                    for (Integer right : partition(input.substring(i + 1))) {
+                for (int left : partition(input.substring(0, i))) {
+                    for (int right : partition(input.substring(i + 1))) {
                         if (input.charAt(i) == '+') {
-                            result.add(left + right);
+                            res.add(left + right);
                         } else if (input.charAt(i) == '-') {
-                            result.add(left - right);
-                        } else if (input.charAt(i) == '*') {
-                            result.add(left * right);
+                            res.add(left - right);
+                        } else {
+                            res.add(left * right);
                         }
                     }
                 }
             }
         }
-        return result;
+        return res;
     }
 
     public static void main(String[] args) {
