50b966d5be5e7fa06a9ae74289d91cea67abbff2
zhangwei112 <zhangwei112@jd.com>
2020-08-10 17:29:09 +0800

    呵呵

--- problems/src/tree/InorderTraversal.java
+++ problems/src/tree/InorderTraversal.java
@@ -31,18 +31,34 @@ public class InorderTraversal {
         }
     }
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List < Integer > res = new ArrayList < > ();
-        Stack < TreeNode > stack = new Stack < > ();
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
+    public List<Integer> inorderTraversal(TreeNode root) {//中序遍历
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current!=null || !stack.isEmpty()){
+            while (current!=null){
+                stack.add(current);
+                current = current.left;
             }
-            curr = stack.pop();
-            res.add(curr.val);
-            curr = curr.right;
+            current = stack.pop();
+            res.add(current.val);
+            current = current.right;
+        }
+        return res;
+    }
+
+    public List<Integer> inorderTraversal1(TreeNode root) {//先序遍历
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current!=null || !stack.isEmpty()){
+            while (current != null){
+                res.add(current.val);
+                stack.add(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            current = current.right;
         }
         return res;
     }
@@ -51,7 +67,7 @@ public class InorderTraversal {
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
-        List<Integer> list = new InorderTraversal().inorderTraversal(root);
+        List<Integer> list = new InorderTraversal().inorderTraversal1(root);
         System.out.println(list.toString());
     }
 }
