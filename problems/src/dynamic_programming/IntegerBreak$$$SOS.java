d11e03746ed4de3402551155b339de4d97a378fe
zhangwei112 <zhangwei112@jd.com>
2020-08-14 11:06:44 +0800

    呵呵

--- problems/src/dynamic_programming/IntegerBreak$$$SOS.java
+++ problems/src/dynamic_programming/IntegerBreak$$$SOS.java
@@ -27,14 +27,11 @@ public class IntegerBreak$$$SOS {
     public int integerBreak(int n) {
         if(n <= 3) return n-1;
         int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
+        dp[1] = 1;dp[2] = 1;
         for(int i = 3; i <= n; i++) {
-            int max = Integer.MIN_VALUE;
             for(int j = 1; j < i; j++) {
-                max = Math.max(dp[j]*dp[i-j], max);
+                dp[i] = Math.max(dp[i], Math.max(j*(i-j),j*dp[i-j]));
             }
-            dp[i] = max;
         }
         return dp[n];
     }
