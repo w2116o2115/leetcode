b19b838e14ad156ac230257cba8cfacb43f3520e
zhangwei112 <zhangwei112@jd.com>
2020-08-14 10:32:58 +0800

    呵呵

--- problems/src/dynamic_programming/CountSubstrings.java
+++ problems/src/dynamic_programming/CountSubstrings.java
@@ -23,22 +23,22 @@ public class CountSubstrings {
         int res = 0;
         int n = s.length();
 
-        // dp[i][j] 表示[i,j]的字符是否为回文子串
-        boolean[][] dp = new boolean[n][n];
+        boolean[][] dp = new  boolean[n][n];
 
-        // 注意，外层循环要倒着写，内层循环要正着写
-        // 因为要求dp[i][j] 需要知道dp[i+1][j-1]
-        for(int i=n-1; i>=0; i--){
-            for(int j=i; j<n; j++){
-
-                // (s.charAt(i)==s.charAt(j) 时，当元素个数为1,2,3个时，一定为回文子串
-                if(s.charAt(i)==s.charAt(j) && (j-i<=2 || dp[i+1][j-1])){
-                    dp[i][j] = true;
+        for (int len = 1;len <= n;len++){
+            for (int start=0;start<n;start++){
+                int end = start+len-1;
+                if (end >= n) continue;
+                if (len == 1 || (len ==2 || dp[start+1][end-1]) && s.charAt(start) == s.charAt(end)){
+                    dp[start][end] = true;
                     res++;
                 }
             }
         }
-
         return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CountSubstrings().countSubstrings("aaa"));
     }
 }
