9bdb178cbe6e77480446a280e80fbe64023cc0c1
zhangwei112 <zhangwei112@jd.com>
2020-08-17 11:17:59 +0800

    呵呵

--- problems/src/dynamic_programming/LongestCommonSubsequence$SOS.java
+++ problems/src/dynamic_programming/LongestCommonSubsequence$SOS.java
@@ -36,21 +36,27 @@ import java.util.Map;
  * ① {s1,s2,s3..si}和 {t1,t2,t3...tj-1} 最大子序列
  *
  * ② {s1,s2,s3...si-1}和{t1,t2,t3....tj} 最大子序列
+ *
+ *  dp[i][j] 表示 s1 前 i个字母  和  s2 前j个字母  最大子序
  */
 public class LongestCommonSubsequence$SOS {
     public int longestCommonSubsequence(String text1, String text2) {
-        char[] s1 = text1.toCharArray();
-        char[] s2 = text2.toCharArray();
-        int[][] dp = new int[s1.length+1][s2.length+1];
-        for (int i=1;i<s1.length;i++){
-            for (int j=1;j<s2.length;j++){
-                if (s1[i-1] == s2[j-1]){
-                    dp[i][j] = dp[i-1][j-1] +1;
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
                 }else {
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return dp[s1.length][s2.length];
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestCommonSubsequence$SOS().longestCommonSubsequence("abcde","ace"));
     }
 }
