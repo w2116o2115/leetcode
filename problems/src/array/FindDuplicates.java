8bcebb495d1d168629a907fb84a0b04f4f148a51
zhangwei112 <zhangwei112@jd.com>
2020-08-19 16:46:07 +0800

    呵呵

--- problems/src/array/FindDuplicates.java
+++ problems/src/array/FindDuplicates.java
@@ -21,40 +21,18 @@ import java.util.List;
  */
 public class FindDuplicates {
     public List<Integer> findDuplicates(int[] nums) {
-        quickSort(0,nums.length-1,nums);
         List<Integer> res = new ArrayList<>();
-        for (int i=1;i<nums.length;i++){
-            if (nums[i] == nums[i-1])
-                res.add(nums[i]);
+        for (int i=0;i<nums.length;i++){
+            if (nums[Math.abs(nums[i])-1] < 0){
+                res.add(Math.abs(nums[i]));
+            }
+            nums[Math.abs(nums[i]) - 1] *= -1;
         }
         return res;
     }
 
-    private void quickSort(int left,int right,int[] nums){
-        if (left > right)
-            return;
-        int start = left;
-        int end = right;
-        int temp = nums[left];
-        while (start!=end){
-            while (start<end && nums[end] >= temp)//要先动end
-                end--;
-            while (start<end && nums[start] <= temp)
-                start++;
-            if (start<end){
-                int t = nums[start];
-                nums[start] = nums[end];
-                nums[end] = t;
-            }
-        }
-        nums[left] = nums[start];
-        nums[start] = temp;
-        quickSort(left,start-1,nums);
-        quickSort(start+1,right,nums);
-    }
-
     public static void main(String[] args) {
-        int[] array = new int[]{1,1};
+        int[] array = new int[]{4,3,2,7,8,2,3,1};
         System.out.println(new FindDuplicates().findDuplicates(array));
     }
 }
