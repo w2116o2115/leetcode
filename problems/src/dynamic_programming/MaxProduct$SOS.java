f50f9e41f123f0ffc3298ae595578929afb3499c
zhangwei112 <zhangwei112@jd.com>
2020-09-03 11:40:50 +0800

    呵呵

--- problems/src/dynamic_programming/MaxProduct$SOS.java
+++ problems/src/dynamic_programming/MaxProduct$SOS.java
@@ -21,15 +21,13 @@ package dynamic_programming;
 public class MaxProduct$SOS {
     public int maxProduct(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        int res = nums[0];
-        int max = nums[0];
+        int res = 0;
         int min = nums[0];
+        int max = nums[0];
         for (int i=1;i<nums.length;i++){
-            int curr_max = Math.max(Math.max(max*nums[i],min*nums[i]),nums[i]);
-            int curr_min = Math.min(Math.min(max*nums[i],min*nums[i]),nums[i]);
-            res = Math.max(res,curr_max);
-            max = curr_max;
-            min = curr_min;
+            max = Math.max(nums[i]*max,nums[i]*min);
+            min = Math.min(nums[i]*max,nums[i]*min);
+            res = Math.max(res,max);
         }
         return res;
     }
