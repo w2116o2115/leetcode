631717014a817207c09a510941280e7560162f8c
zhangwei112 <zhangwei112@jd.com>
2020-09-23 10:55:20 +0800

    呵呵

--- problems/src/leetcode/top250/PreorderTraversal$$retry.java
+++ problems/src/leetcode/top250/PreorderTraversal$$retry.java
@@ -2,6 +2,7 @@ package leetcode.top250;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 144. 二叉树的前序遍历
@@ -34,23 +35,23 @@ public class PreorderTraversal$$retry {
 
 
     public List<Integer> preorderTraversal(TreeNode root) {
-        LinkedList<TreeNode> stack = new LinkedList<>();
-        LinkedList<Integer> output = new LinkedList<>();
-        if (root == null) {
-            return output;
-        }
-
+        Stack<TreeNode> stack = new Stack<>();
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
         stack.add(root);
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pollLast();
-            output.add(node.val);
-            if (node.right != null) {
-                stack.add(node.right);
-            }
-            if (node.left != null) {
-                stack.add(node.left);
-            }
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) stack.add(node.right);
+            if (node.left != null) stack.add(node.left);
         }
-        return output;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(new PreorderTraversal$$retry().preorderTraversal(root));
     }
 }
