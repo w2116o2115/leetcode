b805cb505308d634b79ef02eda66add18e8e9533
zhangwei112 <zhangwei112@jd.com>
2020-10-19 16:56:15 +0800

    呵呵

--- problems/src/array/CanJump$retry.java
+++ problems/src/array/CanJump$retry.java
@@ -21,7 +21,7 @@ package array;
  *
  * 贪心算法： 每一步都要跳最大的距离。最后判断是否到达
  */
-public class CanJump$SOS {
+public class CanJump$retry {
     public boolean canJump(int[] nums) {
         int maxPoint = 0;
         for (int i=0;i<nums.length;i++){
@@ -33,6 +33,6 @@ public class CanJump$SOS {
 
     public static void main(String[] args) {
         int[] nums = new int[]{3,2,1,0,4};
-        System.out.println(new CanJump$SOS().canJump(nums));
+        System.out.println(new CanJump$retry().canJump(nums));
     }
 }
