5e91ea92076fa23acbbd403bf0c71ff083d754ee
zhangwei112 <zhangwei112@jd.com>
2020-10-20 10:38:26 +0800

    呵呵

--- problems/src/array/MaximumSwap$retry.java
+++ problems/src/array/MaximumSwap$retry.java
@@ -50,6 +50,6 @@ public class MaximumSwap$retry {
     }
 
     public static void main(String[] args) {
-        System.out.println(new MaximumSwap$retry().maximumSwap(9973));
+        System.out.println(new MaximumSwap$retry().maximumSwap(5432));
     }
 }
