a98a83e29a2fd48d1a2fb786cab47b0e1710a24a
zhangwei112 <zhangwei112@jd.com>
2020-11-17 10:43:48 +0800

    呵呵

---
+++ problems/src/leetcode/top100/ReverseList$$retry.java
@@ -0,0 +1,40 @@
+package leetcode.top100;
+
+/**
+ * 206. 反转链表
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+public class ReverseList$$retry {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head.next == null) return head;
+        ListNode last =  reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode root = new ReverseList$$retry().reverseList(head);
+        System.out.println(1);
+    }
+}
