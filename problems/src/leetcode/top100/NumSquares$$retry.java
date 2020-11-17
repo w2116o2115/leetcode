3e5c33ad236a9fe6359cbf4078e3862277f07687
zhangwei112 <zhangwei112@jd.com>
2020-11-17 10:22:54 +0800

    呵呵

--- problems/src/leetcode/top100/NumSquares$$retry.java
+++ problems/src/leetcode/top100/NumSquares$$retry.java
@@ -23,7 +23,7 @@ f(n) = 1 + min{
 }
 n = (n-1^2) + 1^2
 */
-public class NumSquares {
+public class NumSquares$$retry {
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         for (int i=1;i<=n;i++){
