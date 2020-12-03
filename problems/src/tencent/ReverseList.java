f73f8c5cdfb6e87ddcbfe3290a07d92e3477f3bf
zhangwei112 <zhangwei112@jd.com>
2020-12-03 10:15:25 +0800

    呵呵

--- problems/src/tencent/ReverseList.java
+++ problems/src/tencent/ReverseList.java
@@ -4,7 +4,7 @@ package tencent;
  * 206. 反转链表
  */
 public class ReverseList {
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -22,5 +22,27 @@ public class ReverseList {
             head = next;
         }
         return pre;
+    }
+
+    public ListNode reve(ListNode head) {
+        if (head.next == null) return head;
+        ListNode tail = reve(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tail;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(-1);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode node = new ReverseList().reve(head);
+        System.out.println(1);
     }
 }
