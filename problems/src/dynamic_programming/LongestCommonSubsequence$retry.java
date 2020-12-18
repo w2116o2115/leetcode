e0645cbe5e71cb6b109c188927d4922300203e5c
zhangwei112 <zhangwei112@jd.com>
2020-12-18 11:29:11 +0800

    呵呵

--- problems/src/dynamic_programming/LongestCommonSubsequence$retry.java
+++ problems/src/dynamic_programming/LongestCommonSubsequence$retry.java
@@ -45,7 +45,7 @@ public class LongestCommonSubsequence$retry {
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 if(text1.charAt(i - 1) == text2.charAt(j - 1)){
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
