b074634e24dbd3bb1fde853d699c51ab2dcf1027
lijun <252160922@qq.com>
2021-02-14 19:51:25 +0800

    你好

---
+++ problems/src/stack/Trap$$retry.java
@@ -0,0 +1,36 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ *
+ * 思路 ： 单调递减栈
+ */
+public class Trap$$retry {
+    public int trap(int[] height) {
+        int ans = 0, current = 0;
+        Stack<Integer> stack = new Stack<>();
+        while (current < height.length) {
+            while (!stack.isEmpty() && height[current] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty())
+                    break;
+                int distance = current - stack.peek() - 1;
+                int bounded_height = Math.min(height[current], height[stack.peek()]) - height[top];
+                ans += distance * bounded_height;
+            }
+            stack.push(current++);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Trap$$retry().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+}
