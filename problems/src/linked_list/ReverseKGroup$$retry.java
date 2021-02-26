b7db4b9427eaeafbe61ba8bf53d53db53ab237b8
zhangwei112 <zhangwei112@jd.com>
2021-02-26 10:50:59 +0800

    呵呵·

---
+++ problems/src/linked_list/ReverseKGroup$$retry.java
@@ -0,0 +1,48 @@
+package linked_list;
+
+/**
+ * @author Carlose wei
+ * @version 1.0
+ * @date 2021/2/26 10:46
+ */
+public class ReverseKGroup$$retry {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+        // 区间 [a, b) 包含 k 个待反转元素
+        ListNode a, b;
+        a = b = head;
+        for (int i = 0; i < k; i++) {
+            // 不足 k 个，不需要反转，base case
+            if (b == null) return head;
+            b = b.next;
+        }
+        // 反转前 k 个元素
+        ListNode newHead = reverse(a, b);
+        // 递归反转后续链表并连接起来
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
+
+    private ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre, cur, nxt;
+        pre = null; cur = a; nxt = a;
+        // while 终止的条件改一下就行了
+        while (cur != b) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        // 返回反转后的头结点
+        return pre;
+    }
+}
